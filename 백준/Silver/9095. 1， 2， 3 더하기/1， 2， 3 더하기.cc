6e6560b8cb683fd0e71f2a2b0e6eef247b62c974
chickenchobab <106914762+chickenchobab@users.noreply.github.com>
2023-09-19 00:45:13 +0900

    [Silver III] Title: 1, 2, 3 더하기, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/9095. 1， 2， 3 더하기/1， 2， 3 더하기.cc
+++ 백준/Silver/9095. 1， 2， 3 더하기/1， 2， 3 더하기.cc
@@ -9,13 +9,12 @@ int main(){
     ios::sync_with_stdio(false);
     cin.tie(0); cout.tie(0);
     cin>>t;
-    while(t--){
-        cin>>n;
-        fill(&dp[1], &dp[n],0);
-        dp[1]=1, dp[2]=2, dp[3]=4;
-        for(int i=4;i<=n;i++){
+    dp[1]=1, dp[2]=2, dp[3]=4;
+        for(int i=4;i<=10;i++){
             dp[i]=dp[i-3]+dp[i-2]+dp[i-1];
         }
+    while(t--){
+        cin>>n;
         cout<<dp[n]<<'\n';
     }   
 

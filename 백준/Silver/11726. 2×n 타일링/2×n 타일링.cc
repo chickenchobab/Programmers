62d90252c1317f9b7e9b6440abba6bb96ba05060
chickenchobab <106914762+chickenchobab@users.noreply.github.com>
2023-09-19 11:14:07 +0900

    [Silver III] Title: 2×n 타일링, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Silver/11726. 2×n 타일링/2×n 타일링.cc
+++ 백준/Silver/11726. 2×n 타일링/2×n 타일링.cc
@@ -1,18 +1,17 @@
 #include <iostream>
+#include <algorithm>
 
 using namespace std;
 
-int n, dp[1001][3];
+int n, dp[1001];
 int main(){
+    ios::sync_with_stdio(false);
+
     cin>>n;
     int mod=10000+1+1+1+1+1+1+1;
-    dp[1][0]=1;
-    dp[1][1]=1;
-    dp[1][2]=0;
-
-    for(int i=2;i<=n;i++){
-        dp[i][0]=dp[i][1]=(dp[i-1][0]+dp[i-1][2])%mod;
-        dp[i][2]=dp[i-1][1];
+    dp[1]=1, dp[2]=2;
+    for(int i=3;i<=n;i++){
+        dp[i]=(dp[i-1]+dp[i-2])%mod;
     }
-    cout<<(dp[n][0]+dp[n][2])%mod;
+    cout<<dp[n];
 }

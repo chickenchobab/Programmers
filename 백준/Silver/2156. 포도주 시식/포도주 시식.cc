e4a9dfa6a80394a6aefa247a187df01ceab96fe1
chickenchobab <106914762+chickenchobab@users.noreply.github.com>
2023-09-20 12:07:12 +0900

    [Silver I] Title: 포도주 시식, Time: 0 ms, Memory: 2136 KB -BaekjoonHub

--- 백준/Silver/2156. 포도주 시식/포도주 시식.cc
+++ 백준/Silver/2156. 포도주 시식/포도주 시식.cc
@@ -4,21 +4,27 @@
 using namespace std;
 
 int n;
-int score[10001];
-long long dp[10001];
+int wine[10001];
+long long d[10001]={0,};
 
 int main(){
+
+    ios::sync_with_stdio(false);
+    cin.tie(0); cout.tie(0);
+
     cin>>n;
     for(int i=1;i<=n;i++){
-        cin>>score[i];
-    }
-    dp[1]=score[1];
-    dp[2]=score[1]+score[2];
-    dp[3]=max(score[1],score[2])+score[3];
-    for(int i=1;i<=n;i++){
-        dp[i]=max(dp[i-2], max(dp[i-4]+score[i-1], dp[i-3]+score[i-1]))+score[i];
+        cin>>wine[i];
     }
 
-    sort(dp+1, dp+n+1);
-    cout<<dp[n];
+    d[0]=0;
+    d[1]=wine[1];
+    d[2]=wine[1]+wine[2];
+
+    for(int i=3;i<=n;i++){
+        d[i]=max(d[i-2], d[i-3]+wine[i-1])+wine[i];
+        d[i]=max(d[i],d[i-1]);
+    }
+
+    cout<<d[n];
 }

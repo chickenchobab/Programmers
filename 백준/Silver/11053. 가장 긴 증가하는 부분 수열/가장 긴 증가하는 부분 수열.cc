67610d406538684ebd17183c288ed0def62cb3ed
chickenchobab <106914762+chickenchobab@users.noreply.github.com>
2023-09-21 11:10:00 +0900

    [Silver II] Title: 가장 긴 증가하는 부분 수열, Time: 0 ms, Memory: 2028 KB -BaekjoonHub

--- 백준/Silver/11053. 가장 긴 증가하는 부분 수열/가장 긴 증가하는 부분 수열.cc
+++ 백준/Silver/11053. 가장 긴 증가하는 부분 수열/가장 긴 증가하는 부분 수열.cc
@@ -2,36 +2,22 @@
 #include <algorithm>
 
 using namespace std;
-
-int n;
-int arr[1001];
-int d[1001];
+int n, arr[1001], dp[1001];
 
 int main(){
 
-    ios::sync_with_stdio(false);
-    cin.tie(0); cout.tie(0);
-
     cin>>n;
-
     for(int i=1;i<=n;i++){
         cin>>arr[i];
-        d[i]=1;
+        dp[i]=1;
     }
-
     for(int i=1;i<=n;i++){
-        for(int j=i+1;j<=n;j++){
-            if(arr[j]>arr[i]){
-                d[j]=max(d[i]+1,d[j]);
-            }
+        for(int j=1;j<=i;j++){
+            if(arr[i]>arr[j]) 
+                dp[i]=max(dp[i],dp[j]+1);
         }
     }
-
-    int m=0;
-    for(int i=1;i<=n;i++){
-        m=max(d[i],m);
-    }
-
-    cout<<m;
+    sort(dp+1, dp+n+1);
+    cout<<dp[n];
 
 }

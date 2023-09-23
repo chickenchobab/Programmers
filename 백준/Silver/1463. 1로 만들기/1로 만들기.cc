9a6dab24a75b749579ab85d141e8347503514703
chickenchobab <106914762+chickenchobab@users.noreply.github.com>
2023-09-23 10:56:31 +0900

    [Silver III] Title: 1로 만들기, Time: 8 ms, Memory: 5928 KB -BaekjoonHub

--- 백준/Silver/1463. 1로 만들기/1로 만들기.cc
+++ 백준/Silver/1463. 1로 만들기/1로 만들기.cc
@@ -1,18 +1,37 @@
 #include <iostream>
 #include <algorithm>
+#define MAX 1000001
 
 using namespace std;
 
-int n, dp[1000001];
+int num;
+int arr[MAX]={0,};
 
 int main(){
-    cin>>n;
-    //dp[1]=1;
-    for(int i=1;i<=n;i++){
-        dp[i]=dp[i-1]+1;
-        if(i%2==0) dp[i]=min(dp[i/2]+1,dp[i]);
-        if(i%3==0) dp[i]=min(dp[i/3]+1, dp[i]);
+
+    ios::sync_with_stdio(false);
+    cin.tie(0); cout.tie(0);
+    cin>>num;
+    
+
+    int n=num;
+    while(n){
+        if(n%3==0) {
+            if(arr[n/3]) arr[n/3]=min(arr[n]+1,arr[n/3]);
+            else arr[n/3]=arr[n]+1;
+        }
+
+        if(n%2==0) {
+            if(arr[n/2]) arr[n/2]=min(arr[n]+1,arr[n/2]);
+            else arr[n/2]=arr[n]+1;
+        }
+
+        if(arr[n-1]) arr[n-1]=min(arr[n]+1,arr[n-1]);
+        else arr[n-1]=arr[n]+1;
+        n--;
     }
-    cout<<dp[n]-1;
+    
+
+    cout<<arr[1];
 
 }

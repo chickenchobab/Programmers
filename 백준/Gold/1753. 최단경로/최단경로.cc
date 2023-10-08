0e1eb3bf8cbc4f24af4ff23f2ea29b186e0dba9f
chickenchobab <106914762+chickenchobab@users.noreply.github.com>
2023-10-09 00:27:51 +0900

    [Gold IV] Title: 최단경로, Time: 104 ms, Memory: 9244 KB -BaekjoonHub

--- 백준/Gold/1753. 최단경로/최단경로.cc
+++ 백준/Gold/1753. 최단경로/최단경로.cc
@@ -1,5 +1,4 @@
 #include <iostream>
-#include <algorithm>
 #include <queue>
 #include <vector>
 #define INF 3333333
@@ -13,17 +12,24 @@ priority_queue<edge, vector<edge>, greater<edge>> pq;
 int d[22222];
 
 int main(){
+    
+    ios::sync_with_stdio(false);
+    cin.tie(0); cout.tie(0);
+
     cin>>n>>e>>s;
     for(int i=1;i<=e;i++){
         cin>>a>>b>>c;
         graph[a].push_back({c,b});
-        if(a==s) {pq.push({c,b}); d[b]=c;}
     }
+    
+    //모든 노드 정보를 pq에 넣음
     for(int i=1;i<=n;i++){
         if(i==s) {pq.push({0,i}); d[i]=0;}
         else {pq.push({INF,i}); d[i]=INF;}
     }
 
+    //d가 작은 노드부터 처리되므로
+    //d가 무한대라면 이미 갱신된 이후에나 등장함
     while(!pq.empty()){
         edge u = pq.top();
         pq.pop();
@@ -40,10 +46,5 @@ int main(){
         else cout<<d[i];
         cout<<'\n';
     }
-
-
-
-    
-
 
 }

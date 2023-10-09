0490ce65720da0cc3ac2109c78b2333a863dcf46
chickenchobab <106914762+chickenchobab@users.noreply.github.com>
2023-10-10 00:42:26 +0900

    [Gold III] Title: 크게 만들기, Time: 16 ms, Memory: 2916 KB -BaekjoonHub

--- 백준/Gold/2812. 크게 만들기/크게 만들기.cc
+++ 백준/Gold/2812. 크게 만들기/크게 만들기.cc
@@ -1,25 +1,29 @@
 #include <iostream>
-#include <deque>
+#include <vector>
 
 using namespace std;
 
 int n,k;
 char ch;
-deque<char> st;
+vector<char> v;
 
 int main(){
+
+    ios::sync_with_stdio(false);
+    cin.tie(0); cout.tie(0);
+
     cin>>n>>k;
     int cnt=k;
     for(int i=1;i<=n;i++){
         cin>>ch;
-        while(cnt && !st.empty() && ch>st.back()){
-            st.pop_back();
+        while(cnt && !v.empty() && ch>v.back()){
+            v.pop_back();
             cnt--;
         }
-        if(st.size()<n-k)
-            st.push_back(ch);
+        if(v.size()<n-k)
+            v.push_back(ch);
     }
-    for(char ch:st){
+    for(char ch:v){
         cout<<ch;
     }
 }

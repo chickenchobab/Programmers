04ba799cc5997f799426b7af963ca8cea0977768
chickenchobab <106914762+chickenchobab@users.noreply.github.com>
2023-10-27 21:50:15 +0900

    [Gold III] Title: 벽 부수고 이동하기, Time: 56 ms, Memory: 11032 KB -BaekjoonHub

--- 백준/Gold/2206. 벽 부수고 이동하기/벽 부수고 이동하기.cc
+++ 백준/Gold/2206. 벽 부수고 이동하기/벽 부수고 이동하기.cc
@@ -2,36 +2,41 @@
 #include <algorithm>
 #include <queue>
 #include <string>
-#include <cstdio>
+#include <tuple>
 
 using namespace std;
 
-typedef pair<int,int> p;
+typedef tuple<int,int,int> p;
 int n,m,map[1001][1001];
-int visited[1001][1001][2], di[]={1,-1,0,0}, dj[]={0,0,1,-1};
-queue<pair<p,int>> q;
+int cnt[1001][1001], di[]={1,-1,0,0}, dj[]={0,0,1,-1};
+queue<p> q;
 
 int bfs(){
-    q.push({{1,1},0});
-    visited[1][1][0]=1;  
+    q.push({1,1,1});
+    cnt[1][1]=0; 
+
     while(!q.empty()){
-        pair<p,int> tmp=q.front();
+
+        p tmp=q.front();
         q.pop();
-        int i=tmp.first.first;
-        int j=tmp.first.second;
-        int block=tmp.second;
-        if(i==n && j==m) return visited[i][j][block];
+        int i=get<0>(tmp);
+        int j=get<1>(tmp);
+        int dist=get<2>(tmp);
+        if(i==n && j==m) return dist;
+        
         for(int d=0;d<4;d++){
             int ni=i+di[d], nj=j+dj[d];
             if(ni>n || ni<1 || nj>m || nj<1) continue;
             if(map[ni][nj]){
-                if(block) continue;
-                visited[ni][nj][1]=visited[i][j][block]+1;
-                q.push({{ni,nj},1});
+                if(cnt[i][j]==1) continue;
+                if(cnt[ni][nj]<=cnt[i][j]+1) continue;
+                cnt[ni][nj]=cnt[i][j]+1;
+                q.push({ni,nj,dist+1});
             }
-            else if(visited[ni][nj][block]==0){
-                visited[ni][nj][block]=visited[i][j][block]+1;
-                q.push({{ni,nj},block});
+            else{
+                if(cnt[ni][nj]<=cnt[i][j]) continue;
+                cnt[ni][nj]=cnt[i][j];
+                q.push({ni,nj,dist+1});
             }
         }
     }
@@ -41,19 +46,17 @@ int bfs(){
 
 int main(){
     ios::sync_with_stdio(false);
-    cin.tie(0); cout.tie(0);
+    cin.tie(0);
 
     cin>>n>>m; 
-    cin.ignore();
 
     for(int i=1;i<=n;i++){
         string str;
-        getline(cin,str);
+        cin>>str;
 
         for(int j=1;j<=m;j++){
-            int tmp=str[j-1]-'0';
-            map[i][j]=tmp;
-            visited[i][j][0]=visited[i][j][1]=0;
+            map[i][j]=str[j-1]-'0';
+            cnt[i][j]=100;
         }
     }
 

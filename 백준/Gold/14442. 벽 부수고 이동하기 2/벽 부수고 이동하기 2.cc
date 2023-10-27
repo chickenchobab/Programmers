765683b365e70b566964a72f12d054bc594e6427
chickenchobab <106914762+chickenchobab@users.noreply.github.com>
2023-10-27 20:56:40 +0900

    [Gold III] Title: 벽 부수고 이동하기 2, Time: 296 ms, Memory: 11708 KB -BaekjoonHub

--- 백준/Gold/14442. 벽 부수고 이동하기 2/벽 부수고 이동하기 2.cc
+++ 백준/Gold/14442. 벽 부수고 이동하기 2/벽 부수고 이동하기 2.cc
@@ -8,36 +8,43 @@ using namespace std;
 
 typedef pair<int,int> p;
 int n,m,k,map[1001][1001];
-int visited[1001][1001][11], di[]={1,-1,0,0}, dj[]={0,0,1,-1};
+int visited[1001][1001], di[]={1,-1,0,0}, dj[]={0,0,1,-1};
 queue<pair<p,int>> q;
+int step;
 
 int bfs(){
-    q.push({{1,1},0});
-    visited[1][1][0]=1; 
+    q.push({{1,1},k});
+    visited[1][1]=k;
 
     while(!q.empty()){
+        step++;
+        int stepcnt = q.size();
+        while(stepcnt--){
+            pair<p,int> tmp=q.front();
+            q.pop();
 
-        pair<p,int> tmp=q.front();
-        q.pop();
-        int i=tmp.first.first;
-        int j=tmp.first.second;
-        int cnt=tmp.second;
-        if(i==n && j==m) return visited[i][j][cnt];
-        
-        for(int d=0;d<4;d++){
-            int ni=i+di[d], nj=j+dj[d];
-            if(ni>n || ni<1 || nj>m || nj<1) continue;
-            if(map[ni][nj]){
-                if(cnt==k) continue;
-                if(visited[ni][nj][cnt+1]) continue;
-                visited[ni][nj][cnt+1]=visited[i][j][cnt]+1;
-                q.push({{ni,nj},cnt+1});
-            }
-            else if(visited[ni][nj][cnt]==0){
-                visited[ni][nj][cnt]=visited[i][j][cnt]+1;
-                q.push({{ni,nj},cnt});
+            int i=tmp.first.first;
+            int j=tmp.first.second;
+            int chance = tmp.second;
+
+            if(i==n && j==m) return step;
+            for(int d=0;d<4;d++){
+                int ni=i+di[d], nj=j+dj[d];
+                if(ni>n || ni<1 || nj>m || nj<1) continue;
+                if(map[ni][nj]){
+                    if(chance==0) continue;
+                    if(visited[ni][nj]>=chance-1) continue;
+                    visited[ni][nj]=chance-1;
+                    q.push({{ni,nj},chance-1});
+                }
+                else {
+                    if(visited[ni][nj]>=chance) continue;
+                    visited[ni][nj]=chance;
+                    q.push({{ni,nj},chance});
+                }
             }
         }
+        
     }
     return -1;
     
@@ -57,7 +64,7 @@ int main(){
         for(int j=1;j<=m;j++){
             map[i][j]=str[j-1]-'0';
             for(int l=0; l<=k; l++){
-                visited[i][j][l]=0;
+                visited[i][j]=-1;
             }
         }
     }

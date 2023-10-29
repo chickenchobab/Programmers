cf6dde0eaea61e1ee3b422f05c6f21de9e1ac6b5
chickenchobab <106914762+chickenchobab@users.noreply.github.com>
2023-10-29 11:42:11 +0900

    [Gold IV] Title: 연구소, Time: 200 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Gold/14502. 연구소/연구소.cc
+++ 백준/Gold/14502. 연구소/연구소.cc
@@ -1,5 +1,4 @@
 #include <iostream>
-#include <queue>
 #include <algorithm>
 
 using namespace std;
@@ -9,67 +8,59 @@ int n,m,map[9][9];
 int di[]={1,-1,0,0}, dj[]={0,0,1,-1}, visited[9][9];
 int ans;
 
-void bfs(){
-
-    queue<p> q;
-    for(int i=1; i<=n; i++){
-        for(int j=1; j<=m; j++){
-            if(map[i][j]==2){
-                visited[i][j]=1;
-                q.push({i,j});
-            }
-            else visited[i][j]=0;
-        }
-    }
-
-    while(q.size()){
-        int i,j;
-        i=q.front().first, j=q.front().second;
-        q.pop();
-        for(int d=0;d<4;d++){
-            int ni=i+di[d], nj=j+dj[d];
-            if(ni<1 || ni>n || nj<1 || nj>m) continue;
-            if(visited[ni][nj] || map[ni][nj]) continue;
-            visited[ni][nj]=1;
-            q.push({ni,nj});
-        }
-    }
-    
-    int cnt=0;
-    for(int i=1; i<=n; i++){
-        for(int j=1; j<=m; j++){
-            if(map[i][j]==0 && visited[i][j]==0) cnt++; 
-        }
-    }
-
-    ans=max(ans, cnt);
-    
-}
-
 void show(){
+    cout<<'\n';
     for(int i=1;i<=n;i++){
-        for(int j=1; j<=m; j++)
-            cout<<map[i][j]<<' ';
+        for(int j=1;j<=m;j++){
+            cout<<visited[i][j]<<' ';
+        }
         cout<<endl;
     }
-    cout<<endl;
+    cout<<"\n\n";
+}
+
+void dfs(int i, int j){
+
+    visited[i][j]=1;
+    for(int d=0;d<4;d++){
+        int ni=i+di[d], nj=j+dj[d];
+        if(ni<1 || ni>n || nj<1 || nj>m) continue;
+        if(visited[ni][nj] || map[ni][nj]) continue;
+        dfs(ni,nj);
+    }
+    
 }
 
 void build(int idx){
     if(idx==3) {
-        bfs(); 
-        //show();
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=m;j++){
+                visited[i][j]=0;
+            }
+        }
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=m;j++){
+                if(map[i][j]==2) dfs(i,j);
+            }
+        }
+        int cnt=0;
+        for(int i=1; i<=n; i++){
+            for(int j=1; j<=m; j++){
+                if(map[i][j]==0 && visited[i][j]==0)
+                cnt++;
+            }     
+        }
+        ans=max(ans,cnt);
         return;
     }
 
     for(int i=1; i<=n; i++){
         for(int j=1; j<=m; j++){
-            if(map[i][j]==0){
-                map[i][j]=1;
-                build(idx+1);
-                map[i][j]=0;
-            }
-        }
+            if(map[i][j]!=0) continue;
+            map[i][j]=1;
+            build(idx+1);
+            map[i][j]=0;
+        }     
     }
 }
 
@@ -85,14 +76,13 @@ int main(){
         }
     }
 
-    for(int i=1;i<=n;i++){
-        for(int j=1;j<=m;j++){
-            if(map[i][j]==0) {
-                map[i][j]=1;
-                build(1);
-                map[i][j]=0;
-            }
-        }
+    for(int i=1; i<=n; i++){
+        for(int j=1; j<=m; j++){
+            if(map[i][j]!=0) continue;
+            map[i][j]=1;
+            build(1);
+            map[i][j]=0;
+        }     
     }
 
     cout<<ans;

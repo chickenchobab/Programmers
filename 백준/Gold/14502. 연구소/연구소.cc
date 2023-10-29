b4895af8eb7d7c4f347882d87ca9352c9d0c8b3d
chickenchobab <106914762+chickenchobab@users.noreply.github.com>
2023-10-29 10:00:35 +0900

    [Gold IV] Title: 연구소, Time: 292 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Gold/14502. 연구소/연구소.cc
@@ -0,0 +1,95 @@
+#include <iostream>
+#include <queue>
+#include <algorithm>
+
+using namespace std;
+
+typedef pair<int, int> p;
+int n,m,map[9][9];
+int di[]={1,-1,0,0}, dj[]={0,0,1,-1}, visited[9][9];
+int ans;
+
+void bfs(){
+
+    queue<p> q;
+    for(int i=1; i<=n; i++){
+        for(int j=1; j<=m; j++){
+            if(map[i][j]==2){
+                visited[i][j]=1;
+                q.push({i,j});
+            }
+            else visited[i][j]=0;
+        }
+    }
+
+    while(q.size()){
+        int i,j;
+        i=q.front().first, j=q.front().second;
+        q.pop();
+        for(int d=0;d<4;d++){
+            int ni=i+di[d], nj=j+dj[d];
+            if(ni<1 || ni>n || nj<1 || nj>m) continue;
+            if(visited[ni][nj] || map[ni][nj]) continue;
+            visited[ni][nj]=1;
+            q.push({ni,nj});
+        }
+    }
+    
+    int cnt=0;
+    for(int i=1; i<=n; i++){
+        for(int j=1; j<=m; j++){
+            if(map[i][j]==0 && visited[i][j]==0) cnt++; 
+        }
+    }
+
+    ans=max(ans, cnt);
+    
+}
+
+void show(){
+    for(int i=1;i<=n;i++){
+        for(int j=1; j<=m; j++)
+            cout<<map[i][j]<<' ';
+        cout<<endl;
+    }
+    cout<<endl;
+}
+
+void build(int idx){
+    if(idx==3) {
+        bfs(); 
+        //show();
+        return;
+    }
+
+    for(int i=1; i<=n; i++){
+        for(int j=1; j<=m; j++){
+            if(map[i][j]==0){
+                map[i][j]=1;
+                build(idx+1);
+                map[i][j]=0;
+            }
+        }
+    }
+}
+
+int main(){
+    cin>>n>>m;
+    for(int i=1;i<=n;i++){
+        for(int j=1; j<=m; j++){
+            cin>>map[i][j];
+        }
+    }
+
+    for(int i=1;i<=n;i++){
+        for(int j=1;j<=m;j++){
+            if(map[i][j]==0) {
+                map[i][j]=1;
+                build(1);
+                map[i][j]=0;
+            }
+        }
+    }
+
+    cout<<ans;
+}

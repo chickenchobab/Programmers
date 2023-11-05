dd0585070d08c5e98bcf51be5817ce3443061bbc
chickenchobab <106914762+chickenchobab@users.noreply.github.com>
2023-11-05 21:05:03 +0900

    [Gold II] Title: 미확인 도착지, Time: 32 ms, Memory: 4460 KB -BaekjoonHub

--- 백준/Gold/9370. 미확인 도착지/미확인 도착지.cc
+++ 백준/Gold/9370. 미확인 도착지/미확인 도착지.cc
@@ -9,8 +9,7 @@ using namespace std;
 typedef pair<int,int> p;
 int test,n,m,t,s,g,h,a,b,d,dgh;
 vector<p> graph[2002];
-int arr[101];
-
+int arr[101], dist[2002];
 
 void reset(){
     for(int i=1;i<=n;i++){
@@ -18,27 +17,26 @@ void reset(){
     }
 }
 
-int shortest_path(int start, int end){
-    int d[2002];
+void dijkstra(){
+    
     priority_queue<p, vector<p>, greater<p>> pq;
     
     for(int i=1;i<=n;i++){
-        d[i]=MAX;
-    }d[start]=0;
+        dist[i]=MAX;
+    }dist[s]=0;
 
-    pq.push({0,start});
+    pq.push({0,s});
     while(pq.size()){
         p u=pq.top();
         pq.pop();
         for(p v: graph[u.second]){
             int uv=v.first;
-            if(d[v.second]>d[u.second]+uv){
-                d[v.second]=d[u.second]+uv;
-                pq.push({d[v.second], v.second});
+            if(dist[v.second]>dist[u.second]+uv){
+                dist[v.second]=dist[u.second]+uv;
+                pq.push({dist[v.second], v.second});
             }
         }
     }
-    return d[end];
 }
 
 int main(){
@@ -50,28 +48,23 @@ int main(){
         reset();
         for(int i=1;i<=m;i++){
             cin>>a>>b>>d;
+            d*=2;
+            if(a==g && b==h) d--;
+            if(a==h && b==g) d--;
             graph[a].push_back({d,b});
             graph[b].push_back({d,a});
-            if(a==g && b==h) dgh=d;
-            if(a==h && b==g) dgh=d;
         }
         for(int i=1;i<=t;i++){
             cin>>arr[i];
         }
         sort(arr+1, arr+t+1);
+        dijkstra();
         for(int i=1;i<=t;i++){
-            int d=shortest_path(s,arr[i]);
-            if(d==MAX) continue;
-            int dsg=shortest_path(s,g);
-            int dsh=shortest_path(s,h);
-            int tmp=MAX;
-            tmp=min(tmp,dsg+dgh+shortest_path(h,arr[i]));
-            tmp=min(tmp,dsh+dgh+shortest_path(g,arr[i]));
-            if(d==tmp) cout<<arr[i]<<' ';
+            if(dist[arr[i]]%2==1) cout<<arr[i]<<' ';
         }
         cout<<'\n';
 
     }
-
+    return 0;
 
 }

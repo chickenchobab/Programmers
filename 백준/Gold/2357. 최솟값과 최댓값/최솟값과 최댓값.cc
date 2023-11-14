a2b00d2e16e9e9649f6f55f038e91854fc451f00
chickenchobab <106914762+chickenchobab@users.noreply.github.com>
2023-11-14 10:04:48 +0900

    [Gold I] Title: 최솟값과 최댓값, Time: 92 ms, Memory: 4076 KB -BaekjoonHub

--- 백준/Gold/2357. 최솟값과 최댓값/최솟값과 최댓값.cc
+++ 백준/Gold/2357. 최솟값과 최댓값/최솟값과 최댓값.cc
@@ -4,68 +4,48 @@
 using namespace std;
 using ll = long long;
 
+typedef pair<int, int> p;
+
 // one-base segment tree
 class Segtree {
  public:
-  vector<ll> tree;
+  vector<p> tree;
   int size;
 
-  Segtree(int t, int option) {
+  Segtree(int t) {
     for (size = 1; size < t; size *= 2)
       ;
-    if(option == 1)
-    tree.resize(size * 2, 1);
-    else
-    tree.resize(size * 2, 1000000000);
+    tree.resize(size * 2, {1000000000, 1});
   }
 
   // update
-  void update1(int pos, ll num) {
+  void update(int pos, int num) {
     int index = size + pos - 1;
-    ll diff = num - tree[index];
     while (index) {
-      tree[index] = max(num, tree[index]);
+      int mn = tree[index].first;
+      int mx = tree[index].second;
+      tree[index] = {min(num, mn), max(num, mx)};
       index /= 2;
     }
   }
 
-  void update2(int pos, ll num) {
-    int index = size + pos - 1;
-    ll diff = num - tree[index];
-    while (index) {
-      tree[index] = min(num, tree[index]);
-      index /= 2;
-    }
-  }
-
- ll query1(int left, int right) { return query1(1, 1, size, left, right); }
- ll query2(int left, int right) { return query2(1, 1, size, left, right); }
-
-  ll query1(int pos, int start, int end, int left, int right) {
+  p query(int left, int right) { return query(1, 1, size, left, right); }
+ 
+  p query(int pos, int start, int end, int left, int right) {
     if (start > right || end < left) {  // 구하려는 구간이 밖에 있는 경우
-      return 1;
+      return {1000000000, 1};
     } else if (left <= start &&
                end <= right) {  // 구하려는 구간이 완전히 안에 있는 경우
       return tree[pos];
     } else {  // 구하려는 구간이 걸쳐 있는 경우
       int mid = (start + end) / 2;
-      return max(query1(pos * 2, start, mid, left, right) ,
-             query1(pos * 2 + 1, mid + 1, end, left, right));
+      p a = query(pos * 2, start, mid, left, right);
+      p b = query(pos * 2 + 1, mid + 1, end, left, right);
+
+      return { min(a.first, b.first), max(a.second, b.second) };
     }
   }
 
-  ll query2(int pos, int start, int end, int left, int right) {
-    if (start > right || end < left) {  // 구하려는 구간이 밖에 있는 경우
-      return 1000000000;
-    } else if (left <= start &&
-               end <= right) {  // 구하려는 구간이 완전히 안에 있는 경우
-      return tree[pos];
-    } else {  // 구하려는 구간이 걸쳐 있는 경우
-      int mid = (start + end) / 2;
-      return min(query2(pos * 2, start, mid, left, right) ,
-             query2(pos * 2 + 1, mid + 1, end, left, right));
-    }
-  }
 };
 
 ll n,m,a,b;
@@ -74,18 +54,20 @@ ll n,m,a,b;
 int main(){
     ios::sync_with_stdio(false);
     cin.tie(0);
+
     cin>>n>>m;
-    Segtree seg1(n, 1);
-    Segtree seg2(n, 2);
+    
+    Segtree seg(n);
+
     int input;
     for(int i=1; i<=n; i++){
         cin>>input;
-        seg1.update1(i,input);
-        seg2.update2(i,input);
+        seg.update(i, input);
     }
     for(int i=1; i<=m; i++){
-        cin>>a>>b;
-        cout<<seg2.query2(a,b)<<' '<<seg1.query1(a,b)<<'\n';
+        cin >> a >> b;
+        p ans = seg.query(a, b);
+        cout << ans.first << ' ' << ans.second << '\n';
     }
 
 

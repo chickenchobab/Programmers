5fb1318cfbb25a477d323d31b0b3bcc889e790d3
chickenchobab <106914762+chickenchobab@users.noreply.github.com>
2023-12-02 20:34:37 +0900

    [Gold III] Title: 웜홀, Time: 20 ms, Memory: 2336 KB -BaekjoonHub

--- 백준/Gold/1865. 웜홀/웜홀.cc
+++ 백준/Gold/1865. 웜홀/웜홀.cc
@@ -9,16 +9,18 @@ using namespace std;
 typedef tuple<int, int, int> iii;
 int n, m, w, s, e, t;
 int d[555];
-int TC, flag;
+int TC;
 vector<iii> edge;
-vector<int> wsp;
 
 bool bellman_ford(){
+
+    d[1] = 0;
+
     for (int i=1; i<=n-1; i++){
         for(iii j : edge){
             int a, b, ab;
             tie(a, b, ab) = {get<0>(j), get<1>(j), get<2>(j)};
-            if(d[a] == INF) continue;
+            //if(d[a] == INF) continue;
             if(d[b] > d[a] + ab) d[b] = d[a] + ab;
         }
     }
@@ -31,13 +33,19 @@ bool bellman_ford(){
 }
 
 int main(){
+
+    ios :: sync_with_stdio(false);
+    cin.tie(0); cout.tie(0);
+
     cin >> TC;
     while (TC--){
 
         cin >> n >> m >> w;
+
+        for (int i=1; i<=n; i++){
+            d[i] = INF;
+        }
         edge.clear();
-        wsp.clear();
-        flag = 0;
 
         for (int i=1; i<=m; i++){
             cin >> s >> e >> t;
@@ -47,20 +55,11 @@ int main(){
         for (int i=1; i<=w; i++){
             cin >> s >> e >> t;
             edge.push_back({s, e, -t});
-            wsp.push_back(s);
-        }
-        
-        for (int st : wsp){
-            for (int i=1; i<=n; i++) d[i] = INF;
-            d[st] = 0;
-            if (!bellman_ford()) {
-                cout << "YES\n";
-                flag = 1;
-                break;
-            }
         }
 
-        if (flag == 0) cout << "NO\n";
+        !bellman_ford() ? cout << "YES\n" : cout << "NO\n";
+        
+
 
     }
     

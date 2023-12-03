e64e2edfeb573cf1f246d246d197701487c29b78
chickenchobab <106914762+chickenchobab@users.noreply.github.com>
2023-12-04 00:35:34 +0900

    [Gold IV] Title: DSLR, Time: 1680 ms, Memory: 2204 KB -BaekjoonHub

--- 백준/Gold/9019. DSLR/DSLR.cc
+++ 백준/Gold/9019. DSLR/DSLR.cc
@@ -1,46 +1,55 @@
 #include <iostream>
 #include <queue>
+#include <stack>
 #include <string>
 
 using namespace std;
 
 int T, A, B;
-int visited[10000];
+int proto[10000];
+char order[10000];
 
 int calculate (int op, int num){
     if (op == 0) return (num * 2) % 10000;
     if (op == 1) return num? num - 1 : 9999;
+    if (num == 0) return 0;
     if (op == 2) return num / 1000 + num % 1000 * 10;
     if (op == 3) return num / 10 + num % 10 * 1000;
 }
 
-string alpha (int op){
-    if (op == 0) return "D";
-    if (op == 1) return "S";
-    if (op == 2) return "L";
-    if (op == 3) return "R";
+char alpha (int op){
+    if (op == 0) return 'D';
+    if (op == 1) return 'S';
+    if (op == 2) return 'L';
+    if (op == 3) return 'R';
+}
+
+void print(int num){
+    if(order[num] == -1) return;
+    print(proto[num]);
+    cout << order[num];
 }
 
 void bfs(){
 
-    queue<pair<int, string>> q;
-    visited[A] = 1;
-    q.push({A, ""});
+    queue<int> q;
+    order[A] = -1;
+    q.push(A);
 
     while(q.size()){
         auto u = q.front();
         q.pop();
-
-        if (u.first == B) {
-            cout << u.second << '\n' ;
+        
+        if (u == B) {
             return;
         }
 
         for (int d = 0 ; d < 4 ; d ++){
-            int v = calculate(d, u.first);
-            if (visited[v]) continue;
-            visited[v] = 1;
-            q.push({v, u.second + alpha(d)});
+            int v = calculate(d, u);
+            if (order[v]) continue;
+            proto[v] = u;
+            order[v] = alpha(d);
+            q.push(v);
         }
     }
 }
@@ -48,14 +57,16 @@ void bfs(){
 int main(){
 
     ios::sync_with_stdio(false);
-    cin.tie(0); 
-    cout.tie(0);
+    // cin.tie(0); 
+    // cout.tie(0);
 
     cin >> T;
     while (T --){
         cin >> A >> B;
-        for (int i=0; i<=9999; i++) visited[i] = 0;
+        for (int i=0; i<=9999; i++) order[i] = 0;
         bfs();
+        print(B);
+        cout << "\n";
     }
     
 }

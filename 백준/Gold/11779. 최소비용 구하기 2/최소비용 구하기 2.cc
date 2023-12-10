2011162172b5f76e663beea0575135f34734f659
chickenchobab <106914762+chickenchobab@users.noreply.github.com>
2023-12-10 11:08:14 +0900

    [Gold III] Title: 최소비용 구하기 2, Time: 32 ms, Memory: 4752 KB -BaekjoonHub

--- 백준/Gold/11779. 최소비용 구하기 2/최소비용 구하기 2.cc
+++ 백준/Gold/11779. 최소비용 구하기 2/최소비용 구하기 2.cc
@@ -51,6 +51,10 @@ void print_path(){
 
 int main(){
 
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+    
     cin >> n >> m;
     for (int i=1; i<=n; i++){
         d[i] = INF;

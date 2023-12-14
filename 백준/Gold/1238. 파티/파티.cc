9a38adffcd52392c2d52f02c4c96d29ebe572f8c
chickenchobab <106914762+chickenchobab@users.noreply.github.com>
2023-12-15 01:37:11 +0900

    [Gold III] Title: 파티, Time: 952 ms, Memory: 5936 KB -BaekjoonHub

--- 백준/Gold/1238. 파티/파티.cc
+++ 백준/Gold/1238. 파티/파티.cc
@@ -1,49 +1,52 @@
 #include <iostream>
 #include <algorithm>
-#include <queue>
-#include <vector>
 #define INF 100000
 
 using namespace std;
 
 typedef pair<int, int> p;
 int n, m, x, a, b, t;
-vector<p> edge[1001];
+int graph[1001][1001];
 
-int dijkstra(int u, int v){
-    priority_queue<p, vector<p>, greater<p>> pq;
-    int d[1001];
-    for (int i=1; i<=n; i++) d[i] = INF;
-    d[u] = 0;
-    pq.push({0, u});
-    while(pq.size()){
-        p i = pq.top();
-        pq.pop();
-        if (d[i.second] < i.first) continue;
-        for (p j : edge[i.second]){
-            int ij = j.first;
-            if (d[j.second] > d[i.second] + ij){
-                d[j.second] = d[i.second] + ij;
-                pq.push(make_pair(d[j.second], j.second));
-                //cout << j.second << ' ';
+int floyd_warshall(){
+    for (int s=1; s<=n; s++){
+        for (int i=1; i<=n; i++){
+            for (int j=1; j<=n; j++){
+                //if (graph[i][s] == INF || graph[s][j] == INF) continue;
+                graph[i][j] = min(graph[i][j], graph[i][s] + graph[s][j]);
             }
         }
-    }
-    return d[v];
-}
-
-int main(){
-    
-    cin >> n >> m >> x;
-    for (int i=1; i<=m; i++){
-        cin >> a >> b >> t;
-        edge[a].push_back({t,b});
     }
 
     int ans = 0;
     for (int i=1; i<=n; i++){
-        ans = max(ans, dijkstra(i, x) + dijkstra(x, i));
+        if (i == x) graph[i][i] = 0;
+        else graph[i][i] = graph[i][x] + graph[x][i];
+        ans = max(ans, graph[i][i]);
     }
 
-    cout << ans;
+    return ans;
+}
+
+int main(){
+
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+    
+    cin >> n >> m >> x;
+
+    for (int i=1; i<=n; i++){
+        for (int j=1; j<=n; j++){
+            graph[i][j] = INF;
+        }
+    }
+
+    for (int i=1; i<=m; i++){
+        cin >> a >> b >> t;
+        graph[a][b] = t;
+    }
+
+    cout << floyd_warshall();
+
 }

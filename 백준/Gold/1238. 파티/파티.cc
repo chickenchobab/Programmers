e3f6f1bf1ff0234fa938a9763d74b11307b18944
chickenchobab <106914762+chickenchobab@users.noreply.github.com>
2023-12-15 15:23:00 +0900

    [Gold III] Title: 파티, Time: 0 ms, Memory: 2344 KB -BaekjoonHub

--- 백준/Gold/1238. 파티/파티.cc
+++ 백준/Gold/1238. 파티/파티.cc
@@ -6,36 +6,39 @@
 
 using namespace std;
 
-typedef pair<pair<int, int>, int> p;
+typedef pair<int, int> p;
 int n, m, x, a, b, t;
-vector<p> edge[1001];
+vector<p> e1[1001], e2[1001];
 priority_queue<p, vector<p>, greater<p>> pq;
-int d0[1001], d1[1001];
+int d1[1001], d2[1001];
 
 int *arr(int num){
-    if (num == 0) return d0;
-    return d1;
+    if (num == 1) return d1;
+    return d2;
+}
+vector<p> * vec(int num){
+    if (num == 1) return e1;
+    return e2;
 }
 
-void dijkstra(){
+void dijkstra(int num){
 
-    for (int i=1; i<=n; i++) d0[i] = d1[i] = INF;
-    d0[x] = d1[x] = 0;
-    pq.push({{0, x}, 0});
-    pq.push({{0, x}, 1});
+    int *d = arr(num);
+    vector<p> * e = vec(num);
+
+    for (int i=1; i<=n; i++) d[i] = INF;
+    d[x] = 0;
+    pq.push({0, x});
 
     while(pq.size()){
         p u = pq.top();
         pq.pop();
-
-        if (arr(u.second)[u.first.second] < u.first.first) continue;
-        for (p v : edge[u.first.second]){
-            if (v.second != u.second) continue;
-            int uv = v.first.first;
-            int *d = arr(v.second);
-            if (d[v.first.second] < uv + d[u.first.second]) continue;
-            d[v.first.second] = uv + d[u.first.second];
-            pq.push({{d[v.first.second], v.first.second}, v.second});
+        if (d[u.second] < u.first) continue;
+        for (p v : e[u.second]){
+            int uv = v.first;
+            if (d[v.second] < d[u.second] + uv) continue;
+            d[v.second] = d[u.second] + uv;
+            pq.push({d[v.second], v.second});
         }
     }
 }
@@ -50,17 +53,16 @@ int main(){
 
     for (int i=1; i<=m; i++){
         cin >> a >> b >> t;
-        edge[a].push_back({{t, b}, 0});
-        edge[b].push_back({{t, a}, 1});
+        e1[a].push_back({t, b});
+        e2[b].push_back({t, a});
     }
 
-    dijkstra();
-    
+    dijkstra(1); dijkstra(2);
+
     int ans = 0;
     for (int i=1; i<=n; i++){
-        ans = max(ans, d0[i] + d1[i]);
+        ans = max(ans, d1[i] + d2[i]);
     }
 
     cout << ans;
-
 }

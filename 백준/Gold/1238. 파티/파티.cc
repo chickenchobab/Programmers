47bdaac5183f971d16d3806dcb0c52042aa4a909
chickenchobab <106914762+chickenchobab@users.noreply.github.com>
2023-12-15 14:54:59 +0900

    [Gold III] Title: 파티, Time: 0 ms, Memory: 2452 KB -BaekjoonHub

--- 백준/Gold/1238. 파티/파티.cc
+++ 백준/Gold/1238. 파티/파티.cc
@@ -1,49 +1,66 @@
 #include <iostream>
 #include <algorithm>
-#include <queue>
 #include <vector>
+#include <queue>
 #define INF 100000
 
 using namespace std;
 
-typedef pair<int, int> p;
+typedef pair<pair<int, int>, int> p;
 int n, m, x, a, b, t;
 vector<p> edge[1001];
+priority_queue<p, vector<p>, greater<p>> pq;
+int d0[1001], d1[1001];
 
-int dijkstra(int u, int v){
-    priority_queue<p, vector<p>, greater<p>> pq;
-    int d[1001];
-    for (int i=1; i<=n; i++) d[i] = INF;
-    d[u] = 0;
-    pq.push({0, u});
+int *arr(int num){
+    if (num == 0) return d0;
+    return d1;
+}
+
+void dijkstra(){
+
+    for (int i=1; i<=n; i++) d0[i] = d1[i] = INF;
+    d0[x] = d1[x] = 0;
+    pq.push({{0, x}, 0});
+    pq.push({{0, x}, 1});
+
     while(pq.size()){
-        p i = pq.top();
+        p u = pq.top();
         pq.pop();
-        if (d[i.second] < i.first) continue;
-        for (p j : edge[i.second]){
-            int ij = j.first;
-            if (d[j.second] > d[i.second] + ij){
-                d[j.second] = d[i.second] + ij;
-                pq.push(make_pair(d[j.second], j.second));
-                //cout << j.second << ' ';
-            }
+
+        if (arr(u.second)[u.first.second] < u.first.first) continue;
+        for (p v : edge[u.first.second]){
+            if (v.second != u.second) continue;
+            int uv = v.first.first;
+            int *d = arr(v.second);
+            if (d[v.first.second] < uv + d[u.first.second]) continue;
+            d[v.first.second] = uv + d[u.first.second];
+            pq.push({{d[v.first.second], v.first.second}, v.second});
         }
     }
-    return d[v];
 }
 
 int main(){
+
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
     
     cin >> n >> m >> x;
+
     for (int i=1; i<=m; i++){
         cin >> a >> b >> t;
-        edge[a].push_back({t,b});
+        edge[a].push_back({{t, b}, 0});
+        edge[b].push_back({{t, a}, 1});
     }
 
+    dijkstra();
+    
     int ans = 0;
     for (int i=1; i<=n; i++){
-        ans = max(ans, dijkstra(i, x) + dijkstra(x, i));
+        ans = max(ans, d0[i] + d1[i]);
     }
 
     cout << ans;
+
 }

7f616bef4cf7815d16b9f49964a86b244cfb922b
chickenchobab <106914762+chickenchobab@users.noreply.github.com>
2023-12-23 21:31:03 +0900

    [Gold IV] Title: 키 순서, Time: 416 ms, Memory: 3004 KB -BaekjoonHub

---
+++ 백준/Gold/2458. 키 순서/키 순서.cc
@@ -0,0 +1,55 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int n, m, s, d, ans;
+int map[501][501];
+int visited[501], cnt[501];
+
+void reset(){
+    for (int i=1; i<=n; i++)
+        visited[i] = 0;
+}
+
+void dfs(int u){
+    visited[u] = 1;
+    for (int v=1; v<=n; v++){
+        if (map[u][v] != d) continue;
+        if (visited[v]) continue;
+        cnt[s] ++;
+        dfs(v);
+    }
+}
+
+int main(){
+     
+    cin >> n >> m;
+
+    int a, b;
+    for (int i=1; i<=m; i++){
+        cin >> a >> b;
+        map[a][b] = 1;
+        map[b][a] = -1;
+    }
+
+    for (s=1; s<=n; s++){
+        d = 1;
+        dfs(s);
+        reset();
+        d = -1;
+        dfs(s);
+        reset();
+    }
+
+    for (int i=1; i<=n; i++){
+        //cout << cnt[i] << ' '; 
+        if (cnt[i] == n-1) {
+            ans++;
+        }
+    }
+
+    //cout << endl;
+    cout << ans;
+
+}

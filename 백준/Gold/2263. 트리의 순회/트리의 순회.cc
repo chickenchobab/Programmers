ee4e92cf6fac245c0097a60d19ba34923ef736d7
chickenchobab <106914762+chickenchobab@users.noreply.github.com>
2023-12-29 11:08:12 +0900

    [Gold I] Title: 트리의 순회, Time: 32 ms, Memory: 8284 KB -BaekjoonHub

---
+++ 백준/Gold/2263. 트리의 순회/트리의 순회.cc
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int n;
+vector<int> in(100001), post(100001);
+// vector<int> g[100001];
+int index[100001];
+
+void input(){
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+    cin >> n;
+    for (int i = 1; i <= n; i ++) {
+        cin >> in[i];
+        index[in[i]] = i;
+    }
+    for (int i = 1; i <= n; i ++) 
+        cin >> post[i];
+}
+
+void make_tree(int i, int j, int s, int e){
+
+    if (i > j || s > e) return;
+
+    int root = index[post[e]];
+
+    cout << in[root] << ' ';
+    make_tree(i, index[post[e]] - 1, s, s + (root - i) - 1); 
+    make_tree(index[post[e]] + 1, j, s + (root - i), e - 1);
+    
+    return;
+}
+
+
+int main(){
+
+    input();
+    make_tree(1, n, 1, n);
+    
+}

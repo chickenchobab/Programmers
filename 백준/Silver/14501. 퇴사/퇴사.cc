26c42be11c0530cb2f7b36edc02a7d16ce02ed99
chickenchobab <106914762+chickenchobab@users.noreply.github.com>
2024-01-12 14:16:41 +0900

    [Silver III] Title: 퇴사, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/14501. 퇴사/퇴사.cc
+++ 백준/Silver/14501. 퇴사/퇴사.cc
@@ -6,6 +6,8 @@ using namespace std;
 int n,t[16],p[16],dp[16];
 
 int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(0); cout.tie(0);
     cin>>n;
     for(int i=1;i<=n;i++){
         cin>>t[i]>>p[i];
@@ -14,10 +16,6 @@ int main(){
         dp[i]=max(dp[i],dp[i-1]);
         if(i+t[i]-1<=n) dp[i+t[i]-1] = max(dp[i+t[i]-1], dp[i-1]+p[i]);
     }
-
-    // for(int i=1;i<=n;i++){
-    //     cout<<dp[i]<<' ';
-    // }
     
     cout<<dp[n];
 

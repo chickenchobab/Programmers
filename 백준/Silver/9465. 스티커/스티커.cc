a609c939c7dc622710fe54176c2c5d280a78add2
chickenchobab <106914762+chickenchobab@users.noreply.github.com>
2024-01-25 09:40:49 +0900

    [Silver I] Title: 스티커, Time: 84 ms, Memory: 3584 KB -BaekjoonHub

--- 백준/Silver/9465. 스티커/스티커.cc
+++ 백준/Silver/9465. 스티커/스티커.cc
@@ -1,34 +1,47 @@
 #include <iostream>
 #include <algorithm>
-
 using namespace std;
 
-int t, n;
-long long sticker[3][100001];
-long long d[3][100001];
+int t;
+int n;
+int st[2][100001], dp[2][100001];
+
+void init(){
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+    cin >> t;
+}
+
+void input(){
+    cin >> n;
+    for (int i = 0; i < 2; ++ i){
+        for (int j = 1; j <= n; ++ j){
+            cin >> st[i][j];
+            dp[i][j] = 0;
+        }
+    }
+}
+
+int solve(){
+
+    dp[0][1] = st[0][1];
+    dp[1][1] = st[1][1];
+
+    for (int i = 2; i <= n; i ++){
+        dp[0][i] = st[0][i] + max(dp[1][i - 1], dp[1][i - 2]);
+        dp[1][i] = st[1][i] + max(dp[0][i - 1], dp[0][i - 2]);
+    }
+
+    return max(dp[0][n], dp[1][n]);
+}
+
 
 int main(){
-
-    ios::sync_with_stdio(false);
-    cin.tie(0); cout.tie(0);
-
-    cin>>t;
-    while(t--){
-        cin>>n;
-        for(int i=1;i<=2;i++){
-            for(int j=1;j<=n;j++){
-                cin>>sticker[i][j];
-            }
-        }
-
-        d[1][1]=sticker[1][1];
-        d[2][1]=sticker[2][1];
-        for(int i=2;i<=n;i++){
-            d[1][i]=max(d[2][i-1], d[2][i-2])+sticker[1][i];
-            d[2][i]=max(d[1][i-1], d[1][i-2])+sticker[2][i];
-        }
-
-        cout<<max(d[1][n],d[2][n])<<'\n';
-        
+    init();
+    while(t --){
+        input();
+        cout << solve() << '\n';
     }
+    return 0;
 }

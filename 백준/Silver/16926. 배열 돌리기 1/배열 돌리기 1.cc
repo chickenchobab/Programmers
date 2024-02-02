cdcb1d9064fb9658943534d465b6865778f90b2d
chickenchobab <106914762+chickenchobab@users.noreply.github.com>
2024-02-03 01:54:29 +0900

    [Silver I] Title: 배열 돌리기 1, Time: 24 ms, Memory: 2916 KB -BaekjoonHub

--- 백준/Silver/16926. 배열 돌리기 1/배열 돌리기 1.cc
+++ 백준/Silver/16926. 배열 돌리기 1/배열 돌리기 1.cc
@@ -19,48 +19,6 @@ void input(){
     } 
 }
 
-int start_index(int i, int j){
-    int idx;
-    int sub = (m - 1) * i - (n - 1) * j;
-    int add = (m - 1) * i + (n - 1) * j - (n - 1) * (m - 1);
-    if (add * sub < 0) {
-        idx = min(j, (m - 1) - j);
-    }
-    else {
-        idx = min(i, (n - 1) - i);
-    }
-    return idx;
-}
-
-int get_xst_num (int i, int j, int x){
-
-    int cnt = x;
-    int idx = start_index(i, j);
-    int ei = (n - 1) - idx;
-    int ej = (m - 1) - idx;
-    while (cnt){
-        int t;
-        if (i > idx && j == idx) {
-            t =  min(cnt, i - idx);
-            i -= t; 
-        }
-        else if (i == ei && j > idx) {
-            t = min(cnt, j - idx);
-            j -= t;
-        }
-        else if (i < ei && j == ej) {
-            t = min(cnt, ei - i);
-            i += t;
-        }
-        else {
-            t = min(cnt, ej - j);
-            j += t;
-        }
-        cnt -= t;
-    }
-    return arr[i][j];
-}
-
 int main(){
     input();
 
@@ -72,19 +30,15 @@ int main(){
         v[idx].push_back(arr[idx][idx]);
         for (j = idx + 1; j <= (m + 1) - idx; j ++){
             v[idx].push_back(arr[i][j]);
-            //cout << arr[i][j] << ' ';
         }j --;
         for (i = idx + 1; i <= (n + 1) - idx; i ++){
             v[idx].push_back(arr[i][j]);
-            //cout << arr[i][j] << ' ';
         }i --;
         for (j = j - 1; j >= idx; j --){
             v[idx].push_back(arr[i][j]);
-            //cout << arr[i][j] << ' ';
         }j ++;
         for (i = i - 1; i > idx; i --){
             v[idx].push_back(arr[i][j]);
-            // cout << arr[i][j] << ' ';
         }i ++;
     }
 

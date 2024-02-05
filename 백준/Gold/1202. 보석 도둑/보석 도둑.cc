912e4369e356f913383352282fae625cf23ff4d8
chickenchobab <106914762+chickenchobab@users.noreply.github.com>
2024-02-05 14:07:42 +0900

    [Gold II] Title: 보석 도둑, Time: 160 ms, Memory: 8740 KB -BaekjoonHub

--- 백준/Gold/1202. 보석 도둑/보석 도둑.cc
+++ 백준/Gold/1202. 보석 도둑/보석 도둑.cc
@@ -7,21 +7,10 @@ using namespace std;
 typedef pair<int, int> p;
 typedef long long ll;
 
-struct w{
-    bool operator()(p &a, p &b){
-        if (a.first == b.first) return a.second < b.second;
-        return a.first > b.first;
-    }
-};
-struct v{
-    bool operator()(p &a, p &b){
-        if (a.second == b.second) return a.first > b.first;
-        return a.second < b.second;
-    }
-};
-int n, m, capacity[300001];
-priority_queue<p, vector<p>, w> jewel;
-priority_queue<p, vector<p>, v> jewel_capable;
+int n, m;
+int capacity[300001];
+
+p jewel[300001];
 
 void input(){
     ios::sync_with_stdio(false);
@@ -31,7 +20,7 @@ void input(){
     int a, b;
     for (int i = 1; i <= n; i ++){
         cin >> a >> b;
-        jewel.push({a, b});
+        jewel[i] = {a, b};
     }
     for (int i = 1; i <= m; i ++){
         cin >> capacity[i];
@@ -41,17 +30,21 @@ void input(){
 int main(){
     input();
 
+    sort(jewel + 1, jewel + n + 1);
     sort(capacity + 1, capacity + m + 1);
 
     ll ans = 0;
-
+    priority_queue<int> jewel_capable;
+    int idx = 1;
     for (int i = 1; i <= m; i ++){
-        while (jewel.size() && jewel.top().first <= capacity[i]) {
-            jewel_capable.push(jewel.top());
-            jewel.pop();
+
+        while(idx <= n && jewel[idx].first <= capacity[i]) {
+            jewel_capable.push(jewel[idx].second);
+            idx ++;
         }
-        if (jewel_capable.size()){
-            ans += jewel_capable.top().second;
+        
+        if (jewel_capable.size()) {
+            ans += jewel_capable.top();
             jewel_capable.pop();
         }
     }

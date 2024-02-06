63e343ed007b14306b81d8a46204f9c2ee9287a8
chickenchobab <106914762+chickenchobab@users.noreply.github.com>
2024-02-06 21:41:54 +0900

    [Gold I] Title: 로봇 청소기, Time: 4 ms, Memory: 2116 KB -BaekjoonHub

--- 백준/Gold/4991. 로봇 청소기/로봇 청소기.cc
+++ 백준/Gold/4991. 로봇 청소기/로봇 청소기.cc
@@ -2,6 +2,9 @@
 #include <algorithm>
 #include <queue>
 #include <vector>
+#include <tuple>
+#include <cstring>
+#define MAX 444
 
 using namespace std;
 
@@ -9,11 +12,10 @@ int w, h;
 int map[21][21];
 
 typedef pair<int, int> p;
-int idx, ans;
+int cnt, ans, status;
 int di[] = {1, -1, 0, 0}, dj[] = {0, 0, 1, -1};
 
-vector<p> edge[12];
-int visited[12];
+int vis[21][21], graph[11][11], dp[11][1 << 11];
 
 void init(){
     ios::sync_with_stdio(false);
@@ -21,78 +23,92 @@ void init(){
     cout.tie(0);
 }
 
+void reset(){
+    cnt = 1, ans = MAX, status = 0;
+    memset(dp, 0, sizeof(dp));
+    memset(graph, 0, sizeof(graph));
+}
+
 bool input(){
     cin >> w >> h;
     if (w == 0 && h == 0) return false;
-    idx = 1, ans = 444;
+
+    reset();
     char ch;
     for (int i = 1; i <= h; i ++){
         for (int j = 1; j <= w; j ++){
             cin >> ch;
-            if (ch == '\n') cin >> ch;
-            
-            if (ch == '.') map[i][j] = 0;
-            else if (ch == 'o') map[i][j] = 1;
-            else if (ch == 'x') map[i][j] = -1;
-            else map[i][j] = ++ idx;
+            while (ch == '\n' || ch == ' ') cin >> ch;
+
+            if (ch == 'o') map[i][j] = 1;
+            else if (ch == '*') map[i][j] = ++ cnt;
+            else if (ch == '.') map[i][j] = 0;
+            else map[i][j] = -1;
         }
-    }
-    for (int i = 1; i <= 11; i ++) {
-        visited[i] = 0;
-        edge[i].clear();
     }
     return true;
 }
 
-void show(){
-    for (int i = 1; i <= 12; i ++){
-        cout << i << ": ";
-        for (p tmp : edge[i]){
-            cout << "{ distance " << tmp.first << " to " << tmp.second << "} ";
-        }
-        cout << '\n';
-    }
-}
-
-void bfs(int i, int j, int index){
+void bfs(int i, int j, int idx){
     queue<p> q;
-    int visited[21][21];
-    for (int i = 1; i <= h; i ++){
-        for (int j = 1; j <= w; j ++){
-            visited[i][j] = 0;
-        }
-    }
-    
+    int nxt_idx;
+    memset(vis, 0, sizeof(vis));
+
     q.push({i, j});
-    visited[i][j] = 1;
+    vis[i][j] = 1;
+
     while (q.size()){
-        int i = q.front().first;
-        int j = q.front().second;
+        int i = q.front().first, j = q.front().second;
         q.pop();
         for (int d = 0; d < 4; d ++){
-            int ni = i + di[d];
-            int nj = j + dj[d];
+            int ni = i + di[d], nj = j + dj[d];
             if (ni < 1 || ni > h || nj < 1 || nj > w) continue;
-            if (visited[ni][nj]) continue;
-            if (map[ni][nj] == -1) continue;
-            if (map[ni][nj] > index) {
-                edge[index].push_back({visited[i][j], map[ni][nj]});
-                edge[map[ni][nj]].push_back({visited[i][j], index});
+            if (vis[ni][nj]) continue;
+            if ((nxt_idx = map[ni][nj]) == -1) continue;
+
+            if (nxt_idx > idx){
+                graph[idx - 1][nxt_idx - 1] = graph[nxt_idx - 1][idx - 1] = vis[i][j];
             }
             q.push({ni, nj});
-            visited[ni][nj] = visited[i][j] + 1;
+            vis[ni][nj] = vis[i][j] + 1;
         }
     }
 }
 
-void dfs(int cur, int cnt, int dist){
-    if (cnt == idx) ans = min(ans, dist);
-    for (p tmp : edge[cur]){
-        int nxt = tmp.second;
-        if (visited[nxt]) continue;
-        visited[nxt] = 1;
-        dfs(nxt, cnt + 1, dist + tmp.first);
-        visited[nxt] = 0;
+int dfs(int cur, int visited){
+
+    status = max(status, visited);
+
+    if (visited == (1 << cnt) - 1)
+        return 0;
+
+    if (dp[cur][visited])
+        return dp[cur][visited];
+
+    dp[cur][visited] = MAX;
+    for (int nxt = 1; nxt < cnt; nxt ++){
+        if (visited & (1 << nxt)) continue;
+        if (graph[cur][nxt] == 0) continue;
+        dp[cur][visited] = min(dp[cur][visited], dfs(nxt, visited | (1 << nxt)) + graph[cur][nxt]);
+    }
+
+    return dp[cur][visited];
+}
+
+void make_graph(){
+    for (int i = 1; i <= h; i ++){
+        for (int j = 1; j <= w; j ++) {
+            if (map[i][j] > 0) bfs(i, j, map[i][j]);
+        }
+    }
+}
+
+void show_graph(){
+    for (int i = 0; i < cnt; i ++){
+        for (int j = 0; j < cnt; j ++) {
+            cout << graph[i][j] << ' ';
+        }
+        cout << '\n';
     }
 }
 
@@ -100,15 +116,11 @@ int main(){
     init();
     while (1){
         if(!input()) break;
-        for (int i = 1; i <= h; i ++){
-            for (int j = 1; j <= w; j ++){
-                if (map[i][j] >= 1) bfs(i, j, map[i][j]);
-            }
-        }
-        // show();
-        visited[1] = 1;
-        dfs(1, 1, 0);
-        ans == 444 ? cout << -1 : cout << ans;
+        make_graph();
+        // show_graph();
+
+        ans = dfs(0, 1);
+        status == (1 << cnt) - 1 ? cout << ans : cout << -1;
         cout << '\n';
     }
     return 0;

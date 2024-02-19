afd1fb0cfbc59918f40d3fed5548dc3d4b6d0f11
chickenchobab <106914762+chickenchobab@users.noreply.github.com>
2024-02-19 12:06:18 +0900

    [Gold I] Title: 계단 수, Time: 8 ms, Memory: 6020 KB -BaekjoonHub

--- 백준/Gold/1562. 계단 수/계단 수.cc
+++ 백준/Gold/1562. 계단 수/계단 수.cc
@@ -1,20 +1,19 @@
 #include <iostream>
 #include <algorithm>
 #define fastio ios::sync_with_stdio(false);cin.tie(0);cout.tie(0);
-#define div 1000000000
 using namespace std;
 
-typedef long long ll;
 int n;
-ll dp[100][10][1 << 10];
+int divisor = 1000000000;
+int dp[100][10][1 << 10];
 
 void input(){
     fastio
     cin >> n;
 }
 
-ll solve(){
-    ll sum = 0;
+int solve(){
+    int sum = 0;
 
     for (int j = 1; j <= 9; j ++) {
         dp[0][j][1 << j] = 1;
@@ -23,17 +22,17 @@ ll solve(){
     for (int i = 1; i < n; i ++){
         for (int j = 0; j <= 9; j ++){
             for (int k = 1; k <= (1 << 10) - 1; k ++){
-                if (j != 0) dp[i][j][k | (1 << j)] += (dp[i - 1][j - 1][k] % div);
-                if (j != 9) dp[i][j][k | (1 << j)] += (dp[i - 1][j + 1][k] % div);
+                if (j != 0) dp[i][j][k | (1 << j)] = (dp[i][j][k | (1 << j)] + dp[i - 1][j - 1][k]) % divisor;
+                if (j != 9) dp[i][j][k | (1 << j)] = (dp[i][j][k | (1 << j)] + dp[i - 1][j + 1][k]) % divisor;
             }
         }
     }
 
     for (int j = 0; j <= 9; j ++){
-        sum += (dp[n - 1][j][(1 << 10) - 1] % div);
+        sum = (sum + dp[n - 1][j][(1 << 10) - 1]) % divisor;
     }
 
-    return sum % div;
+    return sum % divisor;
 }
 
 int main(){

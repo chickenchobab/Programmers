1338fe742216d6e48686bfcb75483e31f7cd8bd2
chickenchobab <106914762+chickenchobab@users.noreply.github.com>
2024-02-23 18:56:29 +0900

    [Gold IV] Title: 빙산, Time: 60 ms, Memory: 3064 KB -BaekjoonHub

--- 백준/Gold/2573. 빙산/빙산.cc
+++ 백준/Gold/2573. 빙산/빙산.cc
@@ -1,65 +1,93 @@
 #include <iostream>
 #include <algorithm>
+#define fastio ios::sync_with_stdio(false);cin.tie(0);cout.tie(0);
 
 using namespace std;
 
 int n, m, cnt, ans;
-int map[301][301], visited[301][301], water[301][301];
+int height[301][301];
+
+int visited[301][301];
 int di[] = {1, -1, 0, 0}, dj[] = {0, 0, 1, -1};
 
-void reset(){
-    for (int i=1; i<=n; i++){
-        for (int j=1; j<=m; j++){
-            if (map[i][j]) map[i][j] = max(0, map[i][j] - water[i][j]);
-            visited[i][j] = water[i][j] = 0;
+void input(){
+    fastio
+    cin >> n >> m;
+    for (int i = 1; i <= n; i ++){
+        for (int j = 1; j <= m; j ++){
+            cin >> height[i][j];
         }
     }
 }
 
-void dfs(int i, int j){
+void show(){
+    cout << cnt << '\n';
+    for (int i = 1; i <= n; i ++){
+        for (int j = 1; j <= m; j ++){
+            cout << height[i][j] << ' ';
+        }
+        cout << '\n';
+    }
+    cout << '\n';
+}
+
+void check_ice(int i, int j){
     visited[i][j] = 1;
-    int ice = 0;
-    for (int d=0; d<4; d++){
+    for (int d = 0; d < 4; d ++){
         int ni = i + di[d];
         int nj = j + dj[d];
         if (ni < 1 || ni > n || nj < 1 || nj > m) continue;
-        if (visited[ni][nj]) continue;
-        if (map[ni][nj] == 0){
-            water[i][j] ++;
+        if (height[ni][nj] == 0) {
+            visited[i][j] ++;
             continue;
         }
-        dfs(ni, nj);
+        if (visited[ni][nj]) continue;
+        check_ice(ni, nj);
     }
 }
 
+void melt_ice(){
+
+    cnt = 0;
+
+    for (int i = 1; i <= n; i ++){
+        for (int j = 1; j <= m; j ++){
+            visited[i][j] = 0;
+        }
+    }
+
+    for (int i = 1; i <= n; i ++){
+        for (int j = 1; j <= m; j ++){
+            if (!height[i][j] || visited[i][j]) continue;
+            cnt ++;
+            check_ice(i, j);
+        }
+    }
+
+    for (int i = 1; i <= n; i ++){
+        for (int j = 1; j <= m; j ++){
+            if (!height[i][j]) continue;
+            height[i][j] = max(height[i][j] - (visited[i][j] - 1), 0);
+        }
+    }
+
+    ans ++;
+}
+
 int main(){
+    input();
 
-    cin >> n >> m;
-    for (int i=1; i<=n; i++){
-        for (int j=1; j<=m; j++){
-            cin >> map[i][j];
-            visited[i][j] = water[i][j] = 0;
+    while(1){
+        melt_ice();
+        //show();
+        if (cnt > 1) {
+            cout << ans - 1;
+            return 0;
         }
+        if (cnt == 0){
+            cout << 0;
+            return 0;
+        }
+
     }
-
-    while (cnt < 2){
-        cnt = 0;
-        for (int i=1; i<=n; i++){
-            for (int j=1; j<=m; j++){
-                if (!map[i][j]) continue;
-                if (visited[i][j]) continue;
-                dfs(i, j);
-                cnt ++;
-            }
-        }
-        if (cnt == 0) {
-            ans = cnt;
-            break;
-        }
-        if (cnt < 2) ans ++;
-        reset();
-    }
-    
-    cout << ans;
-
 }

79d6b4ece9fe1f2bfafc161b59892213f60c7085
chickenchobab <106914762+chickenchobab@users.noreply.github.com>
2024-02-28 09:58:28 +0900

    [Gold IV] Title: 사이클 게임, Time: 100 ms, Memory: 3972 KB -BaekjoonHub

--- 백준/Gold/20040. 사이클 게임/사이클 게임.cc
+++ 백준/Gold/20040. 사이클 게임/사이클 게임.cc
@@ -1,43 +1,51 @@
 #include <iostream>
-#include <vector>
+#include <algorithm>
+#define fastio ios::sync_with_stdio(false);cin.tie(0);cout.tie(0);
 
 using namespace std;
 
-int n,m,a,b,turn;
-vector<int> set;
+int n, m;
 
-int find(int x){
-    if(set[x]<0) return x;
-    return (set[x]=find(set[x]));
+int parent[500005];
+
+void init(){
+    fastio
+    cin >> n >> m;
+    for (int i = 0; i < n; i ++) parent[i] = -1;
 }
 
-void unite(int a, int b){
-    int c1=find(a), c2=find(b);
-    if(set[c1]<set[c2])
-        set[c2]=c1;
-    else{
-        if(set[c1]==set[c2])
-            set[c2]--;
-        set[c1]=c2;
+int find(int x){
+    if (parent[x] < 0) return x;
+    return parent[x] = find(parent[x]);
+}
+
+void merge(int a, int b){
+    a = find(a), b = find(b);
+
+    if (parent[a] < parent[b])
+        parent[b] = a;
+    else {
+        if (parent[a] == parent[b])
+            parent[b] --;
+        parent[a] = b;
     }
+}
+
+void solve(){
+    int a, b;
+    for (int i = 1; i <= m; i ++){
+        cin >> a >> b;
+        if (find(a) == find(b)) {
+            cout << i << '\n';
+            return;
+        }
+        merge(a, b);
+    }   
+    cout << 0;
 }
 
 int main(){
-    ios::sync_with_stdio(false);
-    cin.tie(0); cout.tie(0);
-    cin>>n>>m;
-    
-    set.assign(n,0);
-    for(int i=0;i<n;i++){
-        set[i]=-1;
-    }
-    for(int i=1;i<=m;i++){
-        cin>>a>>b;
-        if(find(a)==find(b)){
-            turn=i; break;
-        }
-        unite(a,b);
-        
-    }
-    cout<<turn;
+    init();
+    solve();
+    return 0;
 }

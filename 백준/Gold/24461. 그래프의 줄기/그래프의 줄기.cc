d4a6b54703fd61e7aea62580f6eebcabb5bc24c0
chickenchobab <106914762+chickenchobab@users.noreply.github.com>
2024-03-13 10:33:29 +0900

    [Gold II] Title: 그래프의 줄기, Time: 60 ms, Memory: 9620 KB -BaekjoonHub

--- 백준/Gold/24461. 그래프의 줄기/그래프의 줄기.cc
+++ 백준/Gold/24461. 그래프의 줄기/그래프의 줄기.cc
@@ -1,74 +1,69 @@
 #include <iostream>
 #include <algorithm>
 #include <vector>
-#include <queue>
+#include <deque>
+#define fastio ios::sync_with_stdio(false);cin.tie(0);cout.tie(0);
 
 using namespace std;
 
-int n,a,b;
-vector<int> graph[100000];
+int n;
+vector<int> edges[100000];
 
-int flag;
-int edge[100000], turn[100000], cnt[100000], line[100000];
+int connected[100000], turn_idx[100000], turn_cnt[100000];
+deque<int> q;
 vector<int> ans;
 
-int main(){
-    
-    cin>>n;
-
-    for(int i=1;i<=n-1;i++){
-        cin>>a>>b;
-        graph[a].push_back(b);
-        graph[b].push_back(a);
-        edge[a]++; edge[b]++;
+void input(){
+    fastio
+    cin >> n;
+    int a, b;
+    for (int i = 0; i < n - 1; i ++){
+        cin >> a >> b;
+        edges[a].push_back(b);
+        edges[b].push_back(a);
+        connected[a] ++;
+        connected[b] ++;
     }
+}
 
-    queue<int> q;
-
-    for(int i=0;i<n;i++){
-        if(edge[i]==1){
-            q.push(i);
-            turn[i]=1;
-            cnt[turn[i]]++;
-        }
-    }
-
-    while(!q.empty()){
-        int u = q.front();
-        q.pop();
-        edge[u]--;
-
-        if(cnt[turn[u]]<=2) {
-            flag=1;
-            line[u]=1;
-        }
-
-        for(int v: graph[u]){
-
-            if(--edge[v]==1){
-                q.push(v);
-                turn[v]=turn[u]+1;
-                cnt[turn[v]]++;
-                
-                if(flag) line[v]=1;
-            }
-        }
-    }
-
-    for(int i=0;i<n;i++){
-        if(line[i]) ans.push_back(i);
-    }
+void print(){
     sort(ans.begin(), ans.end());
-    for(int i:ans) cout<<i<<' ';
+    for (int tmp : ans) cout << tmp << ' ';
+}
 
+void toposort(){
 
-    
+    for (int i = 0; i < n; i ++) {
+        if (connected[i] == 1) {
+            q.push_back(i);
+            turn_idx[i] = 0;
+            turn_cnt[0] ++;
+        }
+    }
 
-    
+    while (q.size()){
+        int u = q.front();
+        q.pop_front();
+        connected[u] --;
 
+        if (turn_cnt[turn_idx[u]] <= 2) {
+            ans.push_back(u);
+        }
 
+        for (int v : edges[u]){
+            if (-- connected[v] == 1){
+                q.push_back(v);
+                turn_idx[v] = turn_idx[u] + 1;
+                turn_cnt[turn_idx[v]] ++;
+            } 
+        }
+    }
 
+}
 
-
-
+int main(){
+    input();
+    toposort();
+    print();
+    return 0;
 }

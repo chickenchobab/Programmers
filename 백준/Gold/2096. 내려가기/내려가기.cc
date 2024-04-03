5702c177c8dbd9223c0c29fa83ae914f9352f82e
chickenchobab <106914762+chickenchobab@users.noreply.github.com>
2024-04-03 11:09:55 +0900

    [Gold V] Title: 내려가기, Time: 16 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Gold/2096. 내려가기/내려가기.cc
+++ 백준/Gold/2096. 내려가기/내려가기.cc
@@ -1,34 +1,40 @@
 #include <iostream>
 #include <algorithm>
+#define fastio ios::sync_with_stdio(false);cin.tie(0);cout.tie(0);
+
 using namespace std;
 
-typedef pair<int, int> ii;
 int n;
-ii arr[2][3];
+typedef pair<int, int> p;
+p arr[2][3];
 
 void input(){
-    ios::sync_with_stdio(false);
-    cin.tie(0);
-    cout.tie(0);
+    fastio
+    cin >> n;
+}
 
+void solve(){
     int a, b, c;
-    cin >> n >> a >> b >> c;
-    arr[0][0] = {a, a}; arr[0][1] = {b, b}; arr[0][2] = {c, c};
 
-    for (int i = 1; i < n; i ++){
+    for (int i = 1; i <= n; i ++) {
         cin >> a >> b >> c;
-        arr[i % 2][0] = {a + max(arr[(i + 1) % 2][0].first, arr[(i + 1) % 2][1].first), a + min(arr[(i + 1) % 2][0].second, arr[(i + 1) % 2][1].second)};
-        arr[i % 2][1] = {b + max(arr[(i + 1) % 2][0].first, max(arr[(i + 1) % 2][1].first, arr[(i + 1) % 2][2].first)), b + min(arr[(i + 1) % 2][0].second, min(arr[(i + 1) % 2][1].second, arr[(i + 1) % 2][2].second))};
-        arr[i % 2][2] = {c + max(arr[(i + 1) % 2][1].first, arr[(i + 1) % 2][2].first), c + min(arr[(i + 1) % 2][1].second, arr[(i + 1) % 2][2].second)};
+        arr[i % 2][0] = {max(arr[(i + 1) % 2][0].first, arr[(i + 1) % 2][1].first) + a, min(arr[(i + 1) % 2][0].second, arr[(i + 1) % 2][1].second) + a};
+        arr[i % 2][1] = {max(max(arr[(i + 1) % 2][0].first, arr[(i + 1) % 2][1].first), arr[(i + 1) % 2][2].first) + b, min(min(arr[(i + 1) % 2][0].second, arr[(i + 1) % 2][1].second), arr[(i + 1) % 2][2].second) + b};
+        arr[i % 2][2] = {max(arr[(i + 1) % 2][1].first, arr[(i + 1) % 2][2].first) + c, min(arr[(i + 1) % 2][1].second, arr[(i + 1) % 2][2].second) + c};
+        //slide();
     }
+    
+    int mx = 0, mn = 999999;
+    for (int i = 0; i < 3; i ++){
+        mx = max(mx, arr[n % 2][i].first);
+        mn = min(mn, arr[n % 2][i].second);
+    }
+
+    cout << mx << ' ' << mn;
 }
 
 int main(){
     input();
-    int mx = 0, mn = 999999;
-    for (int i = 0; i < 3; i ++){
-        mx = max(mx, arr[(n + 1) % 2][i].first);
-        mn = min(mn, arr[(n + 1) % 2][i].second);
-    }
-    cout << mx << ' ' << mn;
+    solve();
+    return 0;
 }

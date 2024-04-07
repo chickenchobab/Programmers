36fbb7166257ad836e343bf7c36346e37a11088d
chickenchobab <106914762+chickenchobab@users.noreply.github.com>
2024-04-07 19:58:00 +0900

    [Gold IV] Title: 오큰수, Time: 212 ms, Memory: 12496 KB -BaekjoonHub

--- 백준/Gold/17298. 오큰수/오큰수.cc
+++ 백준/Gold/17298. 오큰수/오큰수.cc
@@ -1,32 +1,35 @@
 #include <iostream>
 #include <algorithm>
 #include <deque>
+#include <stack>
 #define fastio ios::sync_with_stdio(false);cin.tie(0);cout.tie(0);
 #define MAX 1000001
 using namespace std;
 
 int n;
 int arr[MAX], nge[MAX];
-deque<int> dq;
+// deque<int> dq;
+stack<int> st;
 
 void input(){
     fastio
     cin >> n;
     for (int i = 1; i <= n; i ++){
         cin >> arr[i];
-        nge[i] = -1;
+        // nge[i] = -1;
     }
 }
 
 void solve(){
 
-    for (int i = 1; i <= n; i ++){
+    for (int i = n; i >= 1; i --){
         
-        while (dq.size() && arr[dq.back()] < arr[i]){
-            nge[dq.back()] = arr[i];
-            dq.pop_back();
+        while (st.size() && st.top() <= arr[i]){
+            st.pop();
         }
-        dq.push_back(i);
+        if (st.size()) nge[i] = st.top();
+        else nge[i] = -1;
+        st.push(arr[i]);
     }
     for (int i = 1; i <= n; i ++){
         cout << nge[i] << ' ';

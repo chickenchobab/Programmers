f0a1821da2cacbe45b2469fdbd841613a2b68ec1
chickenchobab <106914762+chickenchobab@users.noreply.github.com>
2024-05-04 14:07:59 +0900

    [Platinum V] Title: 버블 소트, Time: 116 ms, Memory: 5800 KB -BaekjoonHub

--- 백준/Platinum/1517. 버블 소트/버블 소트.cc
+++ 백준/Platinum/1517. 버블 소트/버블 소트.cc
@@ -6,57 +6,15 @@ using namespace std;
 using etype = int;
 
 int n;
-pair<int, int> arr[500005];
+int arr[500005];
 long long ans;
-// one-base segment tree
-class Segtree {
- public:
-  vector<etype> tree;
-  int size;
-
-  Segtree(int t) {
-    for (size = 1; size < t; size *= 2);
-    tree.resize(size * 2);
-  }
-
-  // update
-  void update(int pos, etype num) {
-    int index = size + pos - 1;
-    tree[index] = num;
-    while (index) {
-      if (tree[index * 2] > tree[index * 2 + 1]) {
-        tree[index] = tree[index * 2];
-        ans ++;
-      }
-      else {
-        tree[index] = tree[index * 2 + 1];
-      }
-      index /= 2;
-    }
-  }
-
-  etype query(int left, int right) { return query(1, 1, size, left, right); }
-
-  etype query(int pos, int start, int end, int left, int right) {
-    if (start > right || end < left) {  // 구하려는 구간이 밖에 있는 경우
-      return 0;
-    } else if (left <= start && end <= right) { // 구하려는 구간이 완전히 안에 있는 경우
-      return tree[pos];
-    } else {  // 구하려는 구간이 걸쳐 있는 경우
-      int mid = (start + end) / 2;
-      return query(pos * 2, start, mid, left, right) +
-             query(pos * 2 + 1, mid + 1, end, left, right);
-    }
-  }
-};
 
 void input(){
     fastio
     cin >> n;
     int num;
     for (int i = 1; i <= n; i ++) {
-      cin >> num;
-      arr[i] = {num, i};
+      cin >> arr[i];
     }
 }
 
@@ -64,22 +22,17 @@ void merge(int p, int q, int r){
   int i = p, j = q + 1, t = 0;
   int tmp[r - p + 1];
   while (i <= q && j <= r){
-    if (arr[i].first <= arr[j].first) {
-      tmp[t ++] = arr[i].first;
-      i ++;
-    }
+    if (arr[i] <= arr[j]) tmp[t ++] = arr[i ++];
     else {
       ans += abs(j - (p + t));
-      tmp[t ++] = arr[j].first;
-      j ++;
+      tmp[t ++] = arr[j ++];
     }
   }
-  while (i <= q) {tmp[t ++] = arr[i].first; i ++;}
-  while (j <= r) {tmp[t ++] = arr[j].first; j ++;}
+  while (i <= q) tmp[t ++] = arr[i ++];
+  while (j <= r) tmp[t ++] = arr[j ++];
   t = 0;
-  for (i = p; i <= r; i ++){
-    arr[i] = {tmp[t ++], i};
-  }
+  for (i = p; i <= r; i ++)
+    arr[i] = tmp[t ++];
 }
 
 void merge_sort(int p, int r){
@@ -92,10 +45,7 @@ void merge_sort(int p, int r){
 }
 
 void solve(){
-    // sort(arr + 1, arr + n + 1);
     merge_sort(1, n);
-    // for (int i = 1; i <= n; i ++) cout << arr[i].first << ' ';
-    // cout << '\n';
     cout << ans;
 }
 

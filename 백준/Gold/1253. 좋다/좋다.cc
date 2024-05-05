43eaab00bd0bdd3777b6ffbfd95c59e46d550608
chickenchobab <106914762+chickenchobab@users.noreply.github.com>
2024-05-05 17:16:39 +0900

    [Gold IV] Title: 좋다, Time: 12 ms, Memory: 2028 KB -BaekjoonHub

--- 백준/Gold/1253. 좋다/좋다.cc
+++ 백준/Gold/1253. 좋다/좋다.cc
@@ -7,14 +7,14 @@ using namespace std;
 
 int n;
 int arr[2002];
-set<int> visited;
+// set<int> visited;
 
 void input(){
     fastio
     cin >> n;
     for (int i = 1; i <= n; i ++) {
         cin >> arr[i];
-        visited.insert(arr[i]);
+        // visited.insert(arr[i]);
     }
 }
 
@@ -23,12 +23,11 @@ void solve(){
     sort(arr + 1, arr + n + 1);
 
     for (int i = 1; i <= n; ++ i){
-        int s = 1, e = n;
+        int s = 1, e = n, sum;
         while (s < e){
-            if (s == i) s ++;
-            if (e == i) e --;
-            if (s >= e) break;
-            int sum = arr[s] + arr[e];
+            if (s == i) {s ++; continue;}
+            if (e == i) {e --; continue;}
+            sum = arr[s] + arr[e];
             if (sum > arr[i]) e --;
             else if (sum < arr[i]) s ++;
             else {

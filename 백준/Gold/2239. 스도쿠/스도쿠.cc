be547ea354608cf695381dae583fc0cc717d1744
chickenchobab <106914762+chickenchobab@users.noreply.github.com>
2024-05-07 17:09:59 +0900

    [Gold IV] Title: 스도쿠, Time: 160 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Gold/2239. 스도쿠/스도쿠.cc
+++ 백준/Gold/2239. 스도쿠/스도쿠.cc
@@ -1,12 +1,13 @@
 #include <iostream>
 #include <algorithm>
+#include <vector>
 #define fastio ios::sync_with_stdio(false);cin.tie(0);cout.tie(0);
 
 using namespace std;
 
-int cnt;
 int map[10][10];
 bool row[10][10], col[10][10], box[3][3][10];
+vector<pair<int, int>> blank;
 void check(int i, int j, int num, bool flag);
 
 void input(){
@@ -18,64 +19,43 @@ void input(){
         for (int j = 1; j <= 9; ++ j){
             num = str[j - 1] - '0';
             if (num) check(i, j, num, 1);
+            else blank.push_back({i, j});
         }
     }
 }
 
 void show(){
     for (int i = 1; i <= 9; ++ i){
-        for (int j = 1; j <= 9; ++ j){
+        for (int j = 1; j <= 9; ++ j)
             cout << map[i][j];
-        }
         cout << '\n';
     }
-    cout << '\n';
 }
 
 void check(int i, int j, int num, bool flag){
-    cnt += (flag ? 1 : -1);
-    // cout << cnt << ' ';
     map[i][j] = (flag) * num;
     row[i][num] = col[j][num] = box[(i - 1) / 3][(j - 1) / 3][num] = flag;
 }
 
-void sudoku(int r, int c){
-    if (cnt == 81) {
+void sudoku(int idx){
+    if (idx == blank.size()) {
         show();
         exit(0);
     }
-    // show();
-    for (int i = 1; i <= 9; ++ i){
-        for (int j = 1; j <= 9; ++ j){
-            if (map[i][j]) continue;
-            for (int num = 1; num <= 9; ++ num){
-                if (row[i][num] || col[j][num] || box[(i - 1) / 3][(j - 1) / 3][num]) continue;
-                check(i, j, num, 1);
-                sudoku(i, j);
-                check(i, j, num, 0);
-            }
-            return;
-        }
+
+    int i, j;
+    pair<int, int> p = blank[idx];
+    i = p.first, j = p.second;
+    for (int num = 1; num <= 9; ++ num){
+        if (row[i][num] || col[j][num] || box[(i - 1) / 3][(j - 1) / 3][num]) continue;
+        check(i, j, num, 1);
+        sudoku(idx + 1);
+        check(i, j, num, 0);
     }
 }
 
 void solve(){
-    if (cnt == 81) {
-        show();
-        return;
-    }
-    for (int i = 1; i <= 9; ++ i){
-        for (int j = 1; j <= 9; ++ j){
-            if (map[i][j]) continue;
-            for (int num = 1; num <= 9; ++ num){
-                if (row[i][num] || col[j][num] || box[(i - 1) / 3][(j - 1) / 3][num]) continue;
-                check(i, j, num, 1);
-                sudoku(i, j);
-                check(i, j, num, 0);
-            }
-            return;
-        }
-    }
+    sudoku(0);
 }
 
 int main(){

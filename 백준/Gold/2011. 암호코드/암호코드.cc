af4565dc96d04367049a28f09b82a4c5595e0e63
chickenchobab <106914762+chickenchobab@users.noreply.github.com>
2024-05-07 18:54:13 +0900

    [Gold V] Title: 암호코드, Time: 0 ms, Memory: 2064 KB -BaekjoonHub

--- 백준/Gold/2011. 암호코드/암호코드.cc
+++ 백준/Gold/2011. 암호코드/암호코드.cc
@@ -1,39 +1,32 @@
 #include <iostream>
 #include <algorithm>
-#include <vector>
 #define fastio ios::sync_with_stdio(false);cin.tie(0);cout.tie(0);
-
+#define DIV 1000000
 using namespace std;
 
+string num;
 int n;
-int num[5005];
 long long dp[5005];
 
 void input(){
     fastio
-    char ch;
-    while ((ch = cin.get()) != '\n') {
-        num[++n] = ch - '0';
-    }
+    cin >> num;
+    n = num.size();
 }
 
 bool solve(){
-    
-    if (num[1] == 0) return false;
+    if (num[0] == '0') return false;
 
     dp[0] = dp[1] = 1;
-
     for (int i = 2; i <= n; i ++){
-        if (num[i] == 0){
-            if (num[i - 1] != 1 && num[i - 1] != 2) return false;
-            dp[i] = (dp[i] + dp[i - 2]) % 1000000;
+        if (num[(i - 1)] == '0'){
+            if (num[(i - 1) - 1] != '1' && num[(i - 1) - 1] != '2') return false;
+            dp[i] = dp[i - 2];
         }
-        else {
-            dp[i] = (dp[i] + dp[i - 1]) % 1000000;
-            if ((num[i] <= 6 && num[i - 1] == 2) || (num[i - 1] == 1)) {
-                dp[i] = (dp[i] + dp[i - 2]) % 1000000;
-            }
-        }
+        else if (num[(i - 1) - 1] == '1' || (num[(i - 1) - 1] == '2' && num[(i - 1)] <= '6'))
+            dp[i] = (dp[i - 1] + dp[i - 2]) % DIV;  
+        else    
+            dp[i] = dp[i - 1];
     }
 
     return true;
@@ -41,7 +34,7 @@ bool solve(){
 
 int main(){
     input();
-    if (solve()) cout << dp[n];
+    if (solve()) cout << dp[n] % DIV;
     else cout << 0;
     return 0;
 }

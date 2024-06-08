0aba238e07b2f7194851d5ac91cc16c319e51c7c
chickenchobab <106914762+chickenchobab@users.noreply.github.com>
2024-06-08 22:14:00 +0900

    [Gold III] Title: ACM Craft, Time: 144 ms, Memory: 2720 KB -BaekjoonHub

--- 백준/Gold/1005. ACM Craft/ACM Craft.cc
+++ 백준/Gold/1005. ACM Craft/ACM Craft.cc
@@ -1,45 +1,74 @@
 #include <iostream>
+#include <algorithm>
 #include <vector>
 #include <queue>
+#define fastio ios::sync_with_stdio(false);cin.tie(0);cout.tie(0);
 
 using namespace std;
+int T;
+int N, K, D[1001];
+int X, Y, W;
+vector<int> edges[1001];
+int ind[1001];
+queue<int> q;
+int dp[1001];
 
-int t,n,k,a,b,w;
-int time[1001],dp[1001];
-vector<int> graph[1001];
+void input(){
+  fastio
+  cin >> T;
+}
+void input0(){
+  cin >> N >> K;
+  for (int i = 1; i <= N; ++ i){
+    cin >> D[i];
+  }
+  for (int i = 1; i <= K; ++ i){
+    cin >> X >> Y;
+    edges[X].push_back(Y);
+    ind[Y] ++;
+  }
+  cin >> W;
+}
 
-int solve(int u){
-    if(dp[u]!=-1)   return dp[u];
-    int res=0;
-    for(int v: graph[u]){
-        res=max(res,solve(v)+time[v]);
+void reset(){
+  for (int i = 1; i <= N; ++ i){
+    edges[i].clear();
+    ind[i] = 0;
+    dp[i] = 0;
+  }
+}
+
+void toposort(){
+  for (int i = 1; i <= N; ++ i){
+    if (ind[i] == 0){
+      dp[i] = D[i];
+      q.push(i);
     }
-    return dp[u]=res;
+  }
+
+  while (q.size()){
+    int cur = q.front();
+    // cout << cur << ' ';
+    q.pop();
+
+    for (int nxt : edges[cur]){
+      if (-- ind[nxt] == 0) q.push(nxt);
+      dp[nxt] = max(dp[nxt], dp[cur] + D[nxt]);
+    }
+  }
+}
+
+void solve(){
+  while (T --){
+    input0();
+    toposort();
+    cout << dp[W] << '\n';
+    reset();
+  }
 }
 
 int main(){
-
-    ios::sync_with_stdio(false);
-    cin.tie(0); cout.tie(0);
-
-    cin>>t;
-    while(t--){
-        cin>>n>>k;
-
-        for(int i=1;i<=n;i++){
-            dp[i]=-1;
-            graph[i].clear();
-            cin>>time[i];
-        }
-
-        for(int i=1;i<=k;i++){
-            cin>>a>>b;
-            graph[b].push_back(a);
-        }
-        cin>>w;
-        solve(w);
-        cout<<dp[w]+time[w]<<'\n';
-    }
-
-    return 0;
+  input();
+  solve();
+  return 0;
 }

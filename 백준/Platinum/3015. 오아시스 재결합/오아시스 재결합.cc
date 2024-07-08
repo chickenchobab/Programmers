983d8b9d7ed7c8060605f4a8e6f32f6540053c23
chickenchobab <106914762+chickenchobab@users.noreply.github.com>
2024-07-08 15:05:59 +0900

    [Platinum V] Title: 오아시스 재결합, Time: 68 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Platinum/3015. 오아시스 재결합/오아시스 재결합.cc
+++ 백준/Platinum/3015. 오아시스 재결합/오아시스 재결합.cc
@@ -15,17 +15,19 @@ void input(){
 void solve(){
   int tmp;
   long long ans = 0;
-  
+
   while (N --){
     cin >> tmp;
     while (st.size() && st.top().first < tmp){
-      ans ++;
+      ans += st.top().second;
       st.pop();
     }
     if (st.size()) {
       if (st.top().first == tmp) {
-        ans += (st.top().second + (st.size() > st.top().second));
-        st.push({tmp, st.top().second + 1});
+        ans += (st.top().second + (st.size() > 1));
+        pair<int, int> p = {tmp, st.top().second + 1};
+        st.pop();
+        st.push(p);
       }
       else {
         ans += 1;

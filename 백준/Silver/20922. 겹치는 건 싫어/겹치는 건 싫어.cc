0c862d35fe076bb039ebbf967ebcaf1d60c78ce3
chickenchobab <106914762+chickenchobab@users.noreply.github.com>
2024-07-09 17:41:53 +0900

    [Silver I] Title: 겹치는 건 싫어, Time: 16 ms, Memory: 3192 KB -BaekjoonHub

--- 백준/Silver/20922. 겹치는 건 싫어/겹치는 건 싫어.cc
+++ 백준/Silver/20922. 겹치는 건 싫어/겹치는 건 싫어.cc
@@ -1,36 +1,33 @@
 #include <iostream>
 #include <algorithm>
 #define fastio ios::sync_with_stdio(false);cin.tie(0);cout.tie(0);
-
 using namespace std;
 
-int n, k;
-int arr[200002];
+int N, K;
+int arr[200002]; 
 int cnt[100001];
 
 void input(){
-    fastio
-    cin >> n >> k;
+  fastio
+  cin >> N >> K;
 }
 
-void solve(){ 
-
-    int s = 1, e = 1, ans = 1;
-    for (e = 1; e <= n; e ++) {
-        cin >> arr[e];
-        cnt[arr[e]] ++;
-        while (cnt[arr[e]] > k) {
-            cnt[arr[s]] --;
-            s ++;
-        }
-        ans = max(ans, e - s + 1);
+void solve(){
+  int s = 0, e = 0, ans = 1;
+  for (e = 0; e < N; ++ e){
+    cin >> arr[e];
+    cnt[arr[e]] ++;
+    while (cnt[arr[e]] > K){
+      cnt[arr[s]] --;
+      s ++;
     }
-    cout << ans;
+    ans = max(ans, e - s + 1);
+  }
+  cout << ans;
 }
-
 
 int main(){
-    input();
-    solve();
-    return 0;
+  input();
+  solve();
+  return 0;
 }

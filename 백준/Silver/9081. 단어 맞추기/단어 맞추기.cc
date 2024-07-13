20c987c49dc88015efe5bb705675130ffb4787f0
chickenchobab <106914762+chickenchobab@users.noreply.github.com>
2024-07-13 21:02:11 +0900

    [Silver I] Title: 단어 맞추기, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Silver/9081. 단어 맞추기/단어 맞추기.cc
+++ 백준/Silver/9081. 단어 맞추기/단어 맞추기.cc
@@ -5,36 +5,36 @@
 
 using namespace std;
 
-int t;
+int T;
+string str;
 
 void input(){
-    fastio
-    cin >> t;
+  fastio
+  cin >> T;
 }
 
 void query(){
-    string str;
-    while (t --){
-        cin >> str;
-        int len = str.length();
-        for (int i = len - 2; i >= 0; i --){
-            if (str[i] >= str[i + 1]) continue; // 내림 차순이 처음으로 깨지는 부분을 찾는다
+  int i, j, len;
+  len = str.length(); 
 
-            int flag = 0;
-            for (int j = len - 1; j > i && flag == 0; j --){
-                if (str[i] >= str[j]) continue; // 정렬 필요한 문자 중에서 i보다 크면서 가장 작은 j를 찾는다
-                flag = 1;
-                // cout << str[i] << ' ' << str[j] << " swapped\n";
-                swap(str[i], str[j]);
-                sort(str.begin() + i + 1, str.end());
-            }
-            if (flag) break;
-        }
-        cout << str << '\n';
-    }
+  for (i = len - 2; i >= 0 && str[i] >= str[i + 1]; i --);
+  if (i == -1) return;
+  for (j = len - 1; j > i && str[i] >= str[j]; j --);
+
+  swap(str[i], str[j]);
+  sort(str.begin() + i + 1, str.end());
+}
+
+void solve(){
+  while (T --){
+    cin >> str;
+    query();
+    cout << str << '\n';
+  }
 }
 
 int main(){
     input();
-    query();
+    solve();
+    return 0;
 }

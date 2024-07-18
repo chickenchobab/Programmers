95a436a6aa5b106d987ff3b633ff858c8862d20b
chickenchobab <106914762+chickenchobab@users.noreply.github.com>
2024-07-18 18:59:21 +0900

    [level 2] Title: 방문 길이, Time: 0.06 ms, Memory: 4.02 MB -BaekjoonHub

--- 프로그래머스/2/49994. 방문 길이/방문 길이.cpp
+++ 프로그래머스/2/49994. 방문 길이/방문 길이.cpp
@@ -1,10 +1,11 @@
 #include <string>
 #include <unordered_map>
+#include <set>
 using namespace std;
 
 int solution(string dirs) {
     int answer = 0;
-    bool visited[11][11][2] = {0,}; // 0:right, 1:down
+    set<pair<int, int>> visited;
     int i = 5, j = 5;
     unordered_map<char, int> dir = {{'L', 0}, {'R', 1}, {'U', 2}, {'D', 3}};
     int di[] = {0, 0, -1, 1}, dj[] = {-1, 1, 0, 0};
@@ -13,35 +14,13 @@ int solution(string dirs) {
         int ni = i + di[dir[ch]];
         int nj = j + dj[dir[ch]];
         if (ni < 0 || ni > 10 || nj < 0 || nj > 10) continue;
-        // i = ni, j = nj;
-        switch (dir[ch]){
-            case 0: 
-                if(!visited[ni][nj][0]){
-                    visited[ni][nj][0] = 1;
-                    answer++;
-                }
-                break;
-            case 1:
-                if(!visited[i][j][0]){
-                    visited[i][j][0] = 1;
-                    answer++;
-                }
-                break;
-            case 2:
-                if (!visited[ni][nj][1]){
-                    visited[ni][nj][1] = 1;
-                    answer++;
-                }
-                break;
-            case 3:
-                if (!visited[i][j][1]){
-                    visited[i][j][1] = 1;
-                    answer++;
-                }
-        }
+        int idx1 = i * 11 + j;
+        int idx2 = ni * 11 + nj;
+        if (idx1 > idx2) swap(idx1, idx2);
+        visited.insert({idx1, idx2});
         i = ni, j = nj;
     }
-    
+    answer = visited.size();
     
     
     

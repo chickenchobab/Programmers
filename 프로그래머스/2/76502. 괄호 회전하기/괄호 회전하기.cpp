2e6a9248a729a7fe3032a72b98af2f2bd06786e5
chickenchobab <106914762+chickenchobab@users.noreply.github.com>
2024-07-19 14:57:25 +0900

    [level 2] Title: 괄호 회전하기, Time: 3.22 ms, Memory: 4.22 MB -BaekjoonHub

---
+++ 프로그래머스/2/76502. 괄호 회전하기/괄호 회전하기.cpp
@@ -0,0 +1,39 @@
+#include <string>
+#include <vector>
+#include <stack>
+#include <iostream>
+
+using namespace std;
+
+bool isRight(string s, int i){
+    stack<char> st;
+    int len = s.length();
+    
+    for (int j = i; j < i + len; ++j){
+        char ch = s[j % len];
+        if (ch == '(' || ch == '{' || ch == '[')
+            st.push(ch);
+        else {
+            if (st.empty()) return false;
+            if (ch == ')' && st.top() != '(') return false;
+            if (ch == ']' && st.top() != '[') return false;
+            if (ch == '}' && st.top() != '{') return false;
+            st.pop();
+        }
+        // st.size() ? cout << st.top() << ' ' : cout << -1 << ' ';
+    }
+    
+    return st.empty();
+}
+
+int solution(string s) {
+    int answer = 0;
+    int len = s.length();
+    
+    for (int i = 0; i < len; ++i){ 
+        if (!(s[i] == '(' || s[i] == '{' || s[i] == '[')) continue;
+        answer += isRight(s, i);
+    }
+    
+    return answer;
+}

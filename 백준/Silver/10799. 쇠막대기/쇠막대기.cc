367de2f37ca123bb1ad335ddb31da01c978c5446
chickenchobab <106914762+chickenchobab@users.noreply.github.com>
2024-07-19 16:17:29 +0900

    [Silver II] Title: 쇠막대기, Time: 0 ms, Memory: 2152 KB -BaekjoonHub

--- 백준/Silver/10799. 쇠막대기/쇠막대기.cc
+++ 백준/Silver/10799. 쇠막대기/쇠막대기.cc
@@ -1,36 +1,47 @@
 #include <iostream>
+#include <algorithm>
 #include <stack>
+#define fastio ios::sync_with_stdio(false);cin.tie(0);cout.tie(0);
 
 using namespace std;
 
-stack<char> st;
-int cnt,sum;
+void input(){
+  fastio
+}
+
+void solve(){
+  int ans = 0;
+  char ch;
+  stack<char> st;
+
+  bool isAroundCanon = 0;
+
+  while (1){
+    ch = cin.get();
+    if (ch == '\n') break;
+
+    if (ch == '(') {
+      st.push(ch);
+      isAroundCanon = 0;
+    }
+    else { // )
+      if (isAroundCanon) {
+        st.pop();
+        ans++;
+      }
+      else {
+        st.pop();
+        ans += st.size();
+        isAroundCanon = 1;
+      }
+    }
+  }
+
+  cout << ans;
+}
 
 int main(){
-
-    char ch;
-    while((ch=cin.get())!='\n'){
-        st.push(ch);
-    }
-
-    while(!st.empty()){
-        if(st.top()==')'){
-            cnt++;
-            st.pop();
-            if(st.top()=='('){
-                cnt--;
-                st.pop();
-                sum+=cnt;
-            }
-        }
-        else{
-            cnt--;
-            st.pop();
-            sum++;
-        }
-    }
-
-    cout<<sum;
-    return 0;
-
+  input();
+  solve();
+  return 0;
 }

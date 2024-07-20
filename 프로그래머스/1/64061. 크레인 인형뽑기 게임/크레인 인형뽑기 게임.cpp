6eaa7427b3c89c1159681084ce251dd4f3371214
chickenchobab <106914762+chickenchobab@users.noreply.github.com>
2024-07-20 10:01:45 +0900

    [level 1] Title: 크레인 인형뽑기 게임, Time: 0.04 ms, Memory: 4.22 MB -BaekjoonHub

--- 프로그래머스/1/64061. 크레인 인형뽑기 게임/크레인 인형뽑기 게임.cpp
+++ 프로그래머스/1/64061. 크레인 인형뽑기 게임/크레인 인형뽑기 게임.cpp
@@ -13,14 +13,13 @@ int solution(vector<vector<int>> board, vector<int> moves) {
         for (int i = 0; i < board.size(); ++i){
             if (!board[i][moves[j] - 1]) continue;
             
-            bool exploded = 0;
-            while (st.size() && st.top() == board[i][moves[j] - 1]){
+            if (st.size() && st.top() == board[i][moves[j] - 1]){
                 st.pop();
-                exploded = 1;
-                answer++;
+                answer += 2;
             }
-            if (exploded) answer++;
-            else st.push(board[i][moves[j] - 1]);
+            else {
+                st.push(board[i][moves[j] - 1]);
+            }
             board[i][moves[j] - 1] = 0;
             break;
         }

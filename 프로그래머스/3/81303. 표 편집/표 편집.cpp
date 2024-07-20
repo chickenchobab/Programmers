76d051679587fd87ee65c77fa9eb294d68315064
chickenchobab <106914762+chickenchobab@users.noreply.github.com>
2024-07-20 11:56:13 +0900

    [level 3] Title: 표 편집, Time: 13.80 ms, Memory: 30 MB -BaekjoonHub

---
+++ 프로그래머스/3/81303. 표 편집/표 편집.cpp
@@ -0,0 +1,64 @@
+#include <string>
+#include <vector>
+#include <stack>
+
+using namespace std;
+
+int stringToNum(string str){
+    int ret = 0, digit = 1;
+    for (int i = str.length() - 1; i >= 0; --i){
+        ret += (str[i] - '0') * digit;
+        digit *= 10;
+    }
+    return ret;
+}
+
+string solution(int n, int k, vector<string> cmd) {
+    string answer = "";
+    stack<int> deleted;
+    vector<int> up(n + 1), down(n + 1);
+    
+    for (int i = 0; i < n; ++i){
+        answer += 'O';
+        up[i] = i - 1;
+        down[i] = i + 1;
+    }
+    
+    int x;
+    for (int c = 0; c < cmd.size(); ++c){
+        switch (cmd[c][0]){
+            case 'U':
+                x = stringToNum(cmd[c].substr(2, cmd[c].length() - 2));
+                while (x--) k = up[k];
+                break;
+            case 'D':
+                x = stringToNum(cmd[c].substr(2, cmd[c].length() - 2));
+                while (x--) k = down[k];
+                break;
+            case 'C':
+                answer[k] = 'X';
+                if (up[k] >= 0)
+                    down[up[k]] = down[k];
+                if (down[k] < n)
+                    up[down[k]] = up[k];
+                deleted.push(k);
+                down[k] < n ? k = down[k] : k = up[k];
+                break;
+            case 'Z':
+                if (deleted.empty()) break;
+                int d = deleted.top();
+                deleted.pop();
+                answer[d] = 'O';
+                int newUp = up[d], newDown = down[d];
+                while (newUp >= 0 && answer[newUp] == 'X') newUp--;
+                while (newDown < n && answer[newDown] == 'X') newDown++;
+                up[d] = newUp;
+                down[d] = newDown;
+                if (up[d] >= 0)  down[up[d]] = d;
+                if (down[d] < n) up[down[d]] = d;
+                break;
+        }
+    }
+    
+    return answer;
+}

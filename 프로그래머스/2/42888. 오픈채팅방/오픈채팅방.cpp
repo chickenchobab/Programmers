61b5082be1b0fb75dd5ab57b98fee6984eb2705b
chickenchobab <106914762+chickenchobab@users.noreply.github.com>
2024-07-23 15:42:38 +0900

    [level 2] Title: 오픈채팅방, Time: 229.85 ms, Memory: 41.2 MB -BaekjoonHub

--- 프로그래머스/2/42888. 오픈채팅방/오픈채팅방.cpp
+++ 프로그래머스/2/42888. 오픈채팅방/오픈채팅방.cpp
@@ -14,7 +14,6 @@ vector<string> solution(vector<string> record) {
         stringstream ss(record[i]);
         
         string action, uid;
-        
         ss >> action >> uid;
         
         if (action == "Enter" || action == "Change"){
@@ -22,18 +21,21 @@ vector<string> solution(vector<string> record) {
             ss >> nickname;
             nicknames[uid] = nickname;
         }
-        
-        if (action == "Enter"){
-            answer.push_back(uid + "님이 들어왔습니다.");
-        }
-        else if (action == "Leave"){
-            answer.push_back(uid + "님이 나갔습니다.");
-        }
     }
     
-    for (int i = 0; i < answer.size(); ++i){
-        int pos = answer[i].find("님");
-        answer[i] = nicknames[answer[i].substr(0, pos)] + answer[i].substr(pos);
+    for (int i = 0; i < record.size(); ++i){
+        
+        stringstream ss(record[i]);
+        
+        string action, uid;
+        ss >> action >> uid;
+        
+        if (action == "Enter"){
+            answer.push_back(nicknames[uid] + "님이 들어왔습니다.");
+        }
+        else if (action == "Leave"){
+            answer.push_back(nicknames[uid] + "님이 나갔습니다.");
+        }
     }
     
     return answer;

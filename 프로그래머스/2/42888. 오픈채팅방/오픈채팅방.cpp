9e810e3c9f2cfb3d3ae3dd5a5124ecb90147ce06
chickenchobab <106914762+chickenchobab@users.noreply.github.com>
2024-07-23 15:38:45 +0900

    [level 2] Title: 오픈채팅방, Time: 250.09 ms, Memory: 47.9 MB -BaekjoonHub

--- 프로그래머스/2/42888. 오픈채팅방/오픈채팅방.cpp
+++ 프로그래머스/2/42888. 오픈채팅방/오픈채팅방.cpp
@@ -7,32 +7,33 @@ using namespace std;
 
 vector<string> solution(vector<string> record) {
     vector<string> answer;
-    unordered_map<string, string> nickname;
+    unordered_map<string, string> nicknames;
     
     for (int i = 0; i < record.size(); ++i){
         
         stringstream ss(record[i]);
-        vector<string> tokens;
-        string token;
-        while (ss >> token){
-            tokens.push_back(token);
+        
+        string action, uid;
+        
+        ss >> action >> uid;
+        
+        if (action == "Enter" || action == "Change"){
+            string nickname;
+            ss >> nickname;
+            nicknames[uid] = nickname;
         }
         
-        if (tokens[0] == "Enter"){
-            answer.push_back(tokens[1] + "님이 들어왔습니다.");
-            nickname[tokens[1]] = tokens[2];
+        if (action == "Enter"){
+            answer.push_back(uid + "님이 들어왔습니다.");
         }
-        else if (tokens[0] == "Leave"){
-            answer.push_back(tokens[1] + "님이 나갔습니다.");
-        }
-        else {
-            nickname[tokens[1]] = tokens[2];
+        else if (action == "Leave"){
+            answer.push_back(uid + "님이 나갔습니다.");
         }
     }
     
     for (int i = 0; i < answer.size(); ++i){
         int pos = answer[i].find("님");
-        answer[i] = nickname[answer[i].substr(0, pos)] + answer[i].substr(pos);
+        answer[i] = nicknames[answer[i].substr(0, pos)] + answer[i].substr(pos);
     }
     
     return answer;

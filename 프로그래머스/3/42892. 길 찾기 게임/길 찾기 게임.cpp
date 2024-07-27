508a6f485a3ac55febc13bff982888f525425bbb
chickenchobab <106914762+chickenchobab@users.noreply.github.com>
2024-07-27 22:33:41 +0900

    [level 3] Title: 길 찾기 게임, Time: 19.79 ms, Memory: 9.14 MB -BaekjoonHub

--- 프로그래머스/3/42892. 길 찾기 게임/길 찾기 게임.cpp
+++ 프로그래머스/3/42892. 길 찾기 게임/길 찾기 게임.cpp
@@ -6,31 +6,27 @@ using namespace std;
 
 vector<vector<int>> answer;
 
-typedef struct Info{
-    int key, x, y;
-}Info;
-vector<Info> nodes;
-
 class Node{
 public:
-    Info info;
-    Node *left, *right;
-    Node(Info i) : info(i), left(nullptr), right(nullptr) {}
+    int key, x, y;
+    Node *left = nullptr;
+    Node *right = nullptr;
+    Node(int key, int x, int y) : key(key), x(x), y(y) {}
 };
 
-Node* makeBinaryTree(Node* root, Info info){
+Node* makeBinaryTree(Node* root, Node* newNode){
     if (!root)
-        return new Node(info);
-    if ((root->info).x > info.x)
-        root->left = makeBinaryTree(root->left, info);
+        return newNode;
+    if (root->x > newNode->x)
+        root->left = makeBinaryTree(root->left, newNode);
     else
-        root->right = makeBinaryTree(root->right, info);
+        root->right = makeBinaryTree(root->right, newNode);
     return root;
 }
 
 void preOrder(Node *node){
     if (!node) return;
-    answer[0].push_back((node->info).key);
+    answer[0].push_back(node->key);
     preOrder(node->left);
     preOrder(node->right);
 }
@@ -39,24 +35,24 @@ void postOrder(Node *node){
     if (!node) return;
     postOrder(node->left);
     postOrder(node->right);
-    answer[1].push_back((node->info).key);
+    answer[1].push_back(node->key);
 }
 
 vector<vector<int>> solution(vector<vector<int>> nodeinfo) {
     
+    vector<Node *> nodes;
     for (int i = 0; i < nodeinfo.size(); ++i){
-        nodes.push_back({i + 1, nodeinfo[i][0], nodeinfo[i][1]});
+        nodes.push_back(new Node(i + 1, nodeinfo[i][0], nodeinfo[i][1]));
     }
     sort(nodes.begin(), nodes.end(),
-        [](Info &a, Info &b) -> bool {
-            if (a.y == b.y)
-                return a.x < b.x;
-            return a.y > b.y;
+        [](Node *a, Node *b) -> bool {
+            if (a->y == b->y) return a->x < b->x;
+            return a->y > b->y;
         });
     
     Node *root = nullptr;
-    for (auto &info : nodes){
-        root = makeBinaryTree(root, info);
+    for (auto node : nodes){
+        root = makeBinaryTree(root, node);
     }
     
     answer.resize(2);

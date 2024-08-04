8f0ce9445b44d53d6cc10954399ad938cb405f5f
chickenchobab <106914762+chickenchobab@users.noreply.github.com>
2024-08-04 11:04:00 +0900

    [level 2] Title: 전력망을 둘로 나누기, Time: 0.03 ms, Memory: 4.21 MB -BaekjoonHub

--- 프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.cpp
+++ 프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.cpp
@@ -16,22 +16,20 @@ int cntChildren(int cur, int prv){
     return children[cur] = ret + 1;
 }
 
-void findOptimalBridge(int cur, int prv){
-    answer = min(answer, abs((N - children[cur]) - children[cur]));
-    for (int &nxt : edges[cur]){
-        if (nxt == prv) continue;
-        findOptimalBridge(nxt, cur);
+void findOptimalBridge(){
+    answer = N;
+    for (int i = 1; i <= N; ++i){
+        answer = min(answer, abs(N - children[i] - children[i]));
     }
 }
 
 int solution(int n, vector<vector<int>> wires) {
-    answer = n;
     N = n;
     for (auto &w : wires){
         edges[w[0]].push_back(w[1]);
         edges[w[1]].push_back(w[0]);
     }
     cntChildren(1, 0); // root is 1 from now on.
-    findOptimalBridge(1, 0);
+    findOptimalBridge();
     return answer;
 }

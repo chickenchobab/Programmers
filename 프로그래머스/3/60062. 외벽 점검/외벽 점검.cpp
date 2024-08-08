9a46ebfba7f4ddb176076666b499cc40e162ad0a
chickenchobab <106914762+chickenchobab@users.noreply.github.com>
2024-08-08 17:23:58 +0900

    [level 3] Title: 외벽 점검, Time: 12.99 ms, Memory: 4.16 MB -BaekjoonHub

--- 프로그래머스/3/60062. 외벽 점검/외벽 점검.cpp
+++ 프로그래머스/3/60062. 외벽 점검/외벽 점검.cpp
@@ -5,49 +5,29 @@
 
 using namespace std;
 
-int answer;
-int N;
-
-int getDistance(vector<int> & weak, int i, int j){
-    i %= weak.size();
-    j %= weak.size();
-    if (weak[i] < weak[j]) return weak[j] - weak[i];
-    return weak[j] + N - weak[i];
-}
-
-void checkWeakPoint(vector<int> &weak, vector<int> &dist, int start, int friendCnt, int weakCnt){
-    // cout << "pos: "<< cur << ' ' << "friend : " << friendCnt << ' ' << "done :" << weakCnt << endl;
-    if (weakCnt == weak.size()){
-        answer = min(answer, friendCnt);
-        return;
-    }
-    if (friendCnt > dist.size())
-        return;
-    
-    int end;
-    int fin = start + (weak.size() - weakCnt);
-    for (end = start; end < fin; ++end){
-        if (dist[friendCnt - 1] < getDistance(weak, start, end + 1)) break;
-    }
-    
-    weakCnt += (end - start);
-    
-    if (weakCnt == weak.size()) 
-        checkWeakPoint(weak, dist, end, friendCnt, weakCnt);
-    else 
-        checkWeakPoint(weak, dist, end + 1, friendCnt + 1, weakCnt + 1);
-}
-
 int solution(int n, vector<int> weak, vector<int> dist) {
-    answer = dist.size() + 1;
-    N = n;
+    int answer = dist.size() + 1;
     
-    do {
-        for (int i = 0; i < weak.size(); ++i)
-            checkWeakPoint(weak, dist, i, 1, 1);
+    int siz = weak.size();
+    for (int i = 0; i < siz; ++i){
+        weak.push_back(weak[i] + n);
+    }
+    
+    do {  
+        for (int start = 0; start < siz; ++start){
+            int friendCnt = 1;
+            int pos = weak[start] + dist[0];
+            
+            for (int nxt = start; nxt < start + siz; ++nxt){
+                if (pos < weak[nxt]) {
+                    ++friendCnt;
+                    if (friendCnt > dist.size()) break;
+                    pos = weak[nxt] + dist[friendCnt - 1];
+                }
+            }
+            answer = min(answer, friendCnt);
+        }
     }while (next_permutation(dist.begin(), dist.end()));
     
-    
-    if (answer == dist.size() + 1) return -1;
-    return answer;
+    return answer == dist.size() + 1 ? -1 : answer;
 }

34ae80f1afe5d37c22b1817dfea31b198b95c8c5
chickenchobab <106914762+chickenchobab@users.noreply.github.com>
2024-08-22 11:25:08 +0900

    [level 4] Title: 단어 퍼즐, Time: 3.49 ms, Memory: 3.78 MB -BaekjoonHub

--- 프로그래머스/4/12983. 단어 퍼즐/단어 퍼즐.cpp
+++ 프로그래머스/4/12983. 단어 퍼즐/단어 퍼즐.cpp
@@ -1,30 +1,25 @@
 #include <iostream>
 #include <string>
 #include <vector>
+#include <unordered_set>
 #include <algorithm>
 #define MAX 22222
 using namespace std;
 
 int solution(vector<string> strs, string t)
 {    
+    unordered_set<string> strsSet(strs.begin(), strs.end());
+    unordered_set<int> sizes;
+    for (const auto &s : strs)
+        sizes.insert(s.length());
     vector<int> dp(t.length() + 1, MAX);
     
-    sort(strs.begin(), strs.end(), [](string &a, string &b){
-        return a.length() < b.length();
-    });
     
     dp[0] = 0;
-    int i, j;
-    for (i = 1; i <= t.length(); ++i){
-        for (string &str : strs){
-            if (str.length() > i) break;
-            // if (str == t.substr(i - str.size(), str.size())){
-            // 	dp[i] = min(dp[i], dp[i - str.size()] + 1);
-            // }
-            int idx = i - str.size();
-            for (j = 0; j < str.length() && str[j] == t[idx + j]; ++j);
-            if (j == str.length())
-                dp[i] = min(dp[i], dp[i - str.size()] + 1);
+    for (int i = 1; i <= t.length(); ++i){
+        for (int size : sizes){
+            if (i >= size && strsSet.count(t.substr(i - size, size)))
+                dp[i] = min(dp[i], dp[i - size] + 1);
         }
     }
     
